c627f7e530ec972c9eb0a3916d817fd34e6f70f5
Rushui Guan <rushui@outlook.com>
2019-10-12 01:11:51 -0400

    get all unit test to work

---
+++ webclient/src/Albatross.CodeGen.WebClient.UnitTest/ValueController.cs
@@ -0,0 +1,24 @@
+﻿using Albatross.WebClient.IntegrationTest.Messages;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Albatross.CodeGen.WebClient.UnitTest {
+	[Route("api/[controller]")]
+	public class ValueController : ControllerBase {
+		[HttpGet("json")]
+		public PayLoad GetJson() {
+			return PayLoadExtension.Make();
+		}
+		[HttpGet("text")]
+		public string GetText() {
+			return PayLoadExtension.GetText();
+		}
+
+        [HttpPost]
+        [Route("post")]
+        public PayLoad Post([FromBody]PayLoad payLoad) {
+            return payLoad;
+        }
+	}
+}
