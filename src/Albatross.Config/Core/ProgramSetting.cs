77503c41a790cc14c5ecf39f3efdd0fd6b0a9a16
Rushui <rushui@outlook.com>
2020-07-17 17:45:15 -0400

    adding service host

--- src/Albatross.Config/Core/ProgramSetting.cs
+++ src/Albatross.Config/Core/ProgramSetting.cs
@@ -6,11 +6,17 @@ using System.Text;
 namespace Albatross.Config.Core {
 	public class ProgramSetting {
 		public const string Key = "program";
+		public const string WindowsServiceManager = "windows";
+		public const string SystemDServiceManager = "systemd";
 
 		static readonly string[] ProductionEnvirionment = new string[] {
 			"prod", "production",
 		};
 
+		/// <summary>
+		/// systemd or windows, used by the Worker host
+		/// </summary>
+		public string ServiceManager { get; set; }
 		public string Environment { get; set; }
 		public bool IsProd => ProductionEnvirionment.Contains(Environment?.ToLower());
         /// <summary>
---
+++ src/Albatross.Host.Worker/ServiceHost.cs
@@ -0,0 +1,55 @@
+﻿using Albatross.Config;
+using Albatross.Config.Core;
+using Albatross.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace Albatross.Host.Worker {
+	public class ServiceHost<T> where T : class, IHostedService {
+		IHostBuilder Create(string[] args) {
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(System.IO.Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.AddEnvironmentVariables()
+				.AddCommandLine(args)
+				.Build();
+
+			var hostBuilder = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+				.UseSerilog();
+			
+			var setting = new GetProgramSetting(configuration).Get();
+			switch (setting.ServiceManager) {
+				case ProgramSetting.WindowsServiceManager:
+					hostBuilder.UseWindowsService();
+					break;
+				case ProgramSetting.SystemDServiceManager:
+					hostBuilder.UseSystemd();
+					break;
+			}
+			hostBuilder.ConfigureAppConfiguration(builder => {
+					builder.Sources.Clear();
+					builder.AddConfiguration(configuration);
+				})
+				.ConfigureServices((hostContext, services) => {
+					services.AddHostedService<T>();
+					ConfigureServices(services);
+				});
+
+			return hostBuilder;
+		}
+		
+
+		public virtual async Task RunAsync(params string[] args) {
+			using var setup = new SetupSerilog();
+			setup.UseConfigFile("serilog.json");
+			await Create(args).Build().RunAsync();
+		}
+
+		public virtual void ConfigureServices(IServiceCollection services) {
+			services.AddConfig<ProgramSetting, GetProgramSetting>();
+		}
+	}
+}
