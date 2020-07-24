226836b8814165239618c532bc386149f5e42195
Rushui <rushui@outlook.com>
2020-07-24 12:18:06 -0400

    update entity

--- src/Albatross.Repository/ByEFCore/ImmutableEntityMap.cs
+++ src/Albatross.Repository/ByEFCore/ImmutableEntityMap.cs
@@ -3,11 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Albatross.Repository.ByEFCore {
-	public class ImmutableEntityMap<T, UserType> : EntityMap<T> where T: ImmutableEntity<UserType> {
+	public class ImmutableEntityMap<T> : EntityMap<T> where T: ImmutableEntity {
 		public virtual string TableName => typeof(T).Name;
 		public override void Map(EntityTypeBuilder<T> builder) {
 			builder.ToTable(TableName);
-			builder.Property(p => p.CreatedBy).IsRequired();
+			builder.Property(p => p.CreatedBy).IsRequired().HasMaxLength(ImmutableEntity.UserNameLength);
 			builder.Property(p => p.CreatedUTC).IsRequired();
 		}
 	}
