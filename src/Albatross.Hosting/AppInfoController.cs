eed9c11dd8b9e741e968eb720d87b62382ccd971
Rushui <rushui@outlook.com>
2020-07-30 11:46:37 -0400

    update logging, config and hosting again

---
+++ src/Albatross.Hosting/AppInfoController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Albatross.Config.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Albatross.Hosting {
+	[Route("api/app-info")]
+	[ApiController]
+	public class AppInfoController : ControllerBase {
+		private readonly ProgramSetting programSetting;
+
+		public AppInfoController(ProgramSetting programSetting) {
+			this.programSetting = programSetting;
+		}
+
+		[HttpGet]
+		public ProgramSetting Get() => programSetting;
+
+		
+
+		[HttpGet("assembly")]
+		public IEnumerable<string> GetAssemblies([FromQuery]string[] name) {
+			return FindAssembly(name);
+		}
+
+		List<string> FindAssembly(IEnumerable<string> names) {
+			List<string> result = new List<string>();
+			foreach (var name in names) {
+				try {
+					result.Add(Assembly.Load(name).FullName);
+				} catch {
+					result.Add($"{name}: not found");
+				}
+			}
+			return result;
+		}
+	}
+}
