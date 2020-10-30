f09089a40cb3dd77bcb0bea000a838b44883a640
Rushui <rushui@outlook.com>
2020-10-30 10:44:05 -0400

    add logging

--- src/Albatross.WebClient/ClientBase.cs
+++ src/Albatross.WebClient/ClientBase.cs
@@ -54,6 +54,7 @@ namespace Albatross.WebClient {
 			return request;
 		}
 		public async Task<string> Invoke(HttpRequestMessage request) {
+			logger.LogInformation("{method}: {url}", request.Method, request.RequestUri);
 			using (var response = await client.SendAsync(request)) {
 				string content = await response.Content.ReadAsStringAsync();
 				if (response.IsSuccessStatusCode) {
