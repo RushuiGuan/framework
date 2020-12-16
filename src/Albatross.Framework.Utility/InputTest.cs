61a02d8e5fa59a7971b832a9d44b04e6f0dbea51
Rushui <rushui@outlook.com>
2020-12-16 14:29:38 -0500

    input test

---
+++ src/Albatross.Framework.Utility/InputTest.cs
@@ -0,0 +1,40 @@
+﻿using Albatross.Hosting.Utility;
+using CommandLine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Albatross.Framework.Utility {
+	[Verb("input-test")]
+	public class InputTestOption{
+		[Option('i', "input", Required =true)]
+		public IEnumerable<int> Array { get; set; }
+
+		[Option('s', "string", Required = false)]
+		public IEnumerable<string> TextArray { get; set; }
+	}
+
+	public class InputTest: MyUtilityBase<InputTestOption> {
+
+		public InputTest(InputTestOption option):base(option) {
+		}
+
+
+		public override Task<int> RunAsync() {
+			if (Options.Array != null) {
+				foreach (var i in Options.Array) {
+					Console.WriteLine(i);
+				}
+			}
+			if(Options.TextArray != null) {
+				foreach (var i in Options.TextArray) {
+					Console.WriteLine(i);
+				}
+			}
+			return Task.FromResult(1);
+		}
+	}
+}
--- src/Albatross.Framework.Utility/Program.cs
+++ src/Albatross.Framework.Utility/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Albatross.Framework.Utility {
 	class Program {
 		static Task<int> Main(string[] args) {
-			return Parser.Default.Run(args, typeof(ReferenceTreeSearch), typeof(LoggingTest), typeof(ShowEnvironment));
+			return Parser.Default.Run(args, typeof(ReferenceTreeSearch).Assembly);
 		}
 	}
 }
