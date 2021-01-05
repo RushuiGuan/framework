23775cbe7874c598d5c8b6e597e13d78c4de5ed1
Rushui <rushui@outlook.com>
2021-01-05 17:57:03 -0500

    add logging capability

--- src/Albatross.Hosting/Startup.cs
+++ src/Albatross.Hosting/Startup.cs
@@ -44,7 +44,7 @@ namespace Albatross.Hosting {
 		public virtual bool Caching { get; } = false;
 
 		public Startup(IConfiguration configuration) {
-			Log.Logger.Information("AspNetCore Startup configuration with secured={secured}, spa={spa}, swagger={swagger}, grpc={grpc}, webapi={webapi}, caching={caching}", Secured, Spa, Swagger, Grpc, WebApi, Caching);
+			LogInfo("AspNetCore Startup configuration with secured={secured}, spa={spa}, swagger={swagger}, grpc={grpc}, webapi={webapi}, caching={caching}", Secured, Spa, Swagger, Grpc, WebApi, Caching);
 			Configuration = configuration;
 			ProgramSetting = new GetProgramSetting(configuration).Get();
 			if (Secured && WebApi) {
@@ -207,6 +207,9 @@ namespace Albatross.Hosting {
 				Message = error.Message,
 				Type = error.GetType().FullName,
 			};
+		}
+		protected void LogInfo(string msgTemplate, params object[] objects) {
+			Log.Logger.Information(msgTemplate, objects);
 		}
 	}
 }
