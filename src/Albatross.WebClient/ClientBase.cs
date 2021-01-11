b3dbe3e06986175bc807f67b8f48eac0cbc79359
Rushui <rushui@outlook.com>
2021-01-11 11:05:49 -0500

    improve logging for web client

--- src/Albatross.WebClient/ClientBase.cs
+++ src/Albatross.WebClient/ClientBase.cs
@@ -55,7 +55,7 @@ namespace Albatross.WebClient {
 			return request;
 		}
 		public async Task<string> Invoke(HttpRequestMessage request) {
-			logger.LogInformation("{method}: {url}", request.Method, $"{client.BaseAddress}{request.RequestUri}");
+			logger.LogInformation("{method}: {url}", request.Method, $"{request.RequestUri.AbsoluteUri}");
 			using (var response = await client.SendAsync(request)) {
 				string content = await response.Content.ReadAsStringAsync();
 				if (response.IsSuccessStatusCode) {
