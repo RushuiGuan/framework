4bd0f22df6534ba53fafeaaa509d3deb1177f4e9
Rushui <rushui@outlook.com>
2021-01-13 09:53:04 -0500

    add codegen module

--- src/Albatross.CodeGen.PowerShell/publish.ps1
+++ src/Albatross.CodeGen.PowerShell/publish.ps1
@@ -1,1 +1,3 @@
-dotnet publish --output c:\tools\modules\codegen
+$path = "C:\dev-tools\modules\codegen"
+Get-ChildItem $path | Remove-Item -Recurse -Force;
+dotnet publish $PSScriptRoot\Albatross.CodeGen.PowerShell.csproj --output $path
