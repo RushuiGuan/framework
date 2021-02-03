68e18036489c6c517f50a2fa84b819f4796102d1
Rushui <rushui@outlook.com>
2021-02-02 22:35:11 -0500

    set the next version

---
+++ tests/Albatross.WebClient.IntegrationTest/TestSerialization.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Xunit;
+using System.Text.Json;
+using System.Linq;
+
+namespace Albatross.WebClient.IntegrationTest {
+	public partial class TestSerialization {
+		public TestSerialization() {
+		}
+
+		[Fact]
+		public void Test() {
+			IEnumerable<string> result = JsonSerializer.Deserialize<IEnumerable<string>>("[\"test\"]");
+			Assert.True(result.Count() == 1);
+
+			ISet<string> result2 = JsonSerializer.Deserialize<ISet<string>>("[\"test\", \"TEST\"]", new JsonSerializerOptions { 
+			});
+			Assert.True(result2.Count() == 1);
+		}
+	}
+}
