d31b63ffb9cf472e900f445b48ebedfd9b952f11
Rushui <rushui@outlook.com>
2021-03-05 23:13:59 -0500

    adding a math library

---
+++ src/Albatross/Math/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Albatross.Math {
+	public static class Extension {
+		public static decimal? ToDecimal(this double? value) {
+			if (value.HasValue) {
+				return Convert.ToDecimal(value.Value);
+			} else {
+				return null;
+			}
+		}
+	}
+}
