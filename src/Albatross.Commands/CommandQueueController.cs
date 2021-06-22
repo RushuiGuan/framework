3342f5c4ff21834a7aa684547d431b639ceb9c29
Rushui <rushui@outlook.com>
2021-06-22 09:50:05 -0400

    add a call to the controller

--- src/Albatross.Commands/CommandQueueController.cs
+++ src/Albatross.Commands/CommandQueueController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Albatross.Commands.Daemon {
 	[Route("api/command-queue")]
@@ -16,6 +17,19 @@ namespace Albatross.Commands.Daemon {
 			return this.commandBus.GetAllQueues();
 		}
 
+		[HttpGet("active")]
+		public IEnumerable<CommandQueueDto> GetActiveCommandQueues() {
+			var names = this.commandBus.GetAllQueues();
+			List<CommandQueueDto> list = new List<CommandQueueDto>();
+			foreach(var name in names) {
+				var queue = commandBus.Get(name).CreateDto();
+				if(queue.Items.Count() > 0) {
+					list.Add(queue);
+				}
+			}
+			return list;
+		}
+
 		[HttpGet("{name}")]
 		public CommandQueueDto GetCommandQueue(string name) {
 			return this.commandBus.Get(name).CreateDto();
@@ -23,5 +37,6 @@ namespace Albatross.Commands.Daemon {
 
 		[HttpPost("{name}")]
 		public void Signal(string name) => this.commandBus.Get(name).Signal();
+
 	}
 }
