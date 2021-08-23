196567e752fbec3172792aaf87e94d7aecac7d90
Rushui <rushui@outlook.com>
2021-08-23 09:18:44 -0400

    adding templating

---
+++ src/Albatross.Templating/Extension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albatross.Templating {
+	public static class Extension {
+		public static IServiceCollection AddTemplating(this IServiceCollection services) {
+			services.AddSingleton<IStringInterpolationService, StringInterpolationService>()
+				.AddSingleton<IRazorTemplateService, RazorTemplateService>();
+			return services;
+		}
+	}
+}
---
+++ src/Albatross.Templating/RazorTemplateService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albatross.Templating {
+	public interface IRazorTemplateService {
+		IActionResult Render<T>(Microsoft.AspNetCore.Mvc.Controller controller, string viewName, T model);
+	}
+	public class RazorTemplateService : IRazorTemplateService {
+		public IActionResult Render<T>(Microsoft.AspNetCore.Mvc.Controller controller,  string viewName, T model) {
+			return controller.View(viewName, model);
+		}
+	}
+}
