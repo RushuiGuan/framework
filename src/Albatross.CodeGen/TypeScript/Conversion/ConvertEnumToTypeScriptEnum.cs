64174eb323f1c4148eebe2934e6e847fb5612042
Rushui <rguan@welton.com>
2022-02-23 01:18:45 -0500

    improve typescript codegen for enum

--- src/Albatross.CodeGen/TypeScript/Conversion/ConvertEnumToTypeScriptEnum.cs
+++ src/Albatross.CodeGen/TypeScript/Conversion/ConvertEnumToTypeScriptEnum.cs
@@ -2,14 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
 
 namespace Albatross.CodeGen.TypeScript.Conversion {
 	public class ConvertEnumToTypeScriptEnum : IConvertObject<Type, TypeScript.Model.Enum> {
 		public TypeScript.Model.Enum Convert(Type type) {
 			if (type.IsEnum) {
-				return new Model.Enum(type.Name) {
+				var model = new Model.Enum(type.Name) {
 					Values = type.GetEnumNames(),
 				};
+				var attrib = type.GetCustomAttribute<JsonConverterAttribute>();
+				model.UseTextValue = attrib?.ConverterType == typeof(JsonStringEnumConverter);
+				return model;
 			} else {
 				throw new InvalidOperationException($"Type {type.Name} is not an Enum type");
 			}
