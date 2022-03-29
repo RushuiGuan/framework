a14ff63318b9772e51aa8a5d392fc5abd2be37df
Rushui <rguan@welton.com>
2022-03-29 13:45:03 -0400

    add extension method for IntArray and StringArray table parameters for dapper

---
+++ src/Albatross.Repository.SqlServer/Extension.cs
@@ -0,0 +1,27 @@
+﻿using Albatross.Repository.SqlServer;
+using Dapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Albatross.Repository.SqlServer {
+	public static class Extension {
+		public static SqlMapper.ICustomQueryParameter AsTableValuedParameter(this IEnumerable<int> values) {
+			DataTable table = new DataTable();
+			table.Columns.Add("Value", typeof(int));
+			foreach(var item in values) {
+				table.Rows.Add(item);
+			}
+			return table.AsTableValuedParameter("dbo.IntArray");
+		}
+
+		public static SqlMapper.ICustomQueryParameter AsTableValuedParameter(this IEnumerable<string> values) {
+			DataTable table = new DataTable();
+			table.Columns.Add("Value", typeof(string));
+			foreach (var item in values) {
+				table.Rows.Add(item);
+			}
+			return table.AsTableValuedParameter("dbo.StringArray");
+		}
+	}
+}
