5485fa2f098aed3a6ae075721b1036c0c77e24ed
rushuiguan2 <rguan@welton.com>
2022-08-03 09:09:31 -0400

    upgraded fully to net6.0

--- tests/Albatross.Repository.Test/EntityTest.cs
+++ tests/Albatross.Repository.Test/EntityTest.cs
@@ -6,7 +6,7 @@ namespace Albatross.Repository.Test {
 	public class EntityTest {
 		[Fact]
 		public void TestMapping() {
-			var entity = new ImmutableEntity(String.Empty);
+			var entity = new ImmutableEntity();
 			Assert.Throws<System.ComponentModel.DataAnnotations.ValidationException>(
 				() => entity.ValidateByDataAnnotations()
 			);
