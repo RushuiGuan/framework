c41329896254af3a1b6b06452c4b8680c21a7daf
Rushui Guan <rguan@welton.com>
2023-02-11 23:56:05 -0500

    add hashcode utilities

---
+++ src/Albatross/Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Albatross {
+	/// <summary>
+	/// Utilities in the root Albatross namespace typically are system utilities that is so low level, it doesn't have a
+	/// category.  Microsoft use namespace "System" for them.  We could consider Albatross.System but Albatross seems fitting.
+	/// </summary>
+	public static class Extension {
+		public static HashCode From<T>(this HashCode hashCode, IEnumerable<T> items) {
+			foreach(var item in items) {
+				hashCode.Add(item);
+			}
+			return hashCode;
+		}
+		public static HashCode From<T>(this HashCode hashCode, T item) {
+			hashCode.Add(item);
+			return hashCode;
+		}
+	}
+}
--- tests/Albatross.Test/RecordClass/TestHashCode.cs
+++ tests/Albatross.Test/RecordClass/TestHashCode.cs
@@ -6,16 +6,35 @@ using System.Threading.Tasks;
 using Xunit;
 
 namespace Albatross.Test.RecordClass {
-	public record class MyRecord {
+	public sealed record class MyDerivedRecord : MyRecord {
 		public string Name { get; set; }
-		public MyRecord(string name) { Name = name; }
+		public MyDerivedRecord(int id) : base(id) {
+		}
+	}
+	public record class MyRecord {
+		public int Id { get; set; }
+		public MyRecord(int id) { Id = id; }
+		public List<string> List { get; init; } = new List<string>();
+		public override int GetHashCode() {
+			var hashCode = new HashCode();
+			hashCode.Add(Id);
+			List.ForEach(args => hashCode.Add(args));
+			return hashCode.ToHashCode();
+		}
+		public virtual bool Equals(MyRecord? other) {
+			if(other == null || other.GetType() != this.GetType()) return false;
+			return Id == other.Id && List.SequenceEqual(other.List);
+		}
 	}
 	public class TestHashCode {
 		[Fact]
 		public void TestBasic() {
-			var x = new MyRecord("a");
-			var y = new MyRecord("a");
+			var x = new MyRecord(1);
+			x.List.Add("a");
+			var y = new MyRecord(1);
+			y.List.Add("a");
 			Assert.Equal(x.GetHashCode(), y.GetHashCode());
+
 			var x_hash = new HashCode();
 			x_hash.Add(x);
 			var x_hashcode = x_hash.ToHashCode();
@@ -25,20 +44,19 @@ namespace Albatross.Test.RecordClass {
 			Assert.Equal(x_hashcode, y_hashcode);
 
 			Assert.NotEqual(x.GetHashCode(), x_hashcode);
-		}
 
+			x.List.Add("b1");
+			y.List.Add("b2");
+			Assert.NotEqual(x.GetHashCode(), y.GetHashCode());
+		}
 		[Fact]
-		public void TestArray() {
-			var x = new MyRecord[0];
-			var y = new MyRecord[0];
+		public void TestDerived() {
+			var x = new MyDerivedRecord(1);
+			var y = new MyDerivedRecord(1);
+			Assert.Equal(x, y);
 			Assert.Equal(x.GetHashCode(), y.GetHashCode());
-			var x_hash = new HashCode();
-			x_hash.Add(x);
-			var x_hashcode = x_hash.ToHashCode();
-			var y_hash = new HashCode();
-			y_hash.Add(y);
-			var y_hashcode = y_hash.ToHashCode();
-			Assert.Equal(x_hashcode, y_hashcode);
+			x.List.Add("b");
+			Assert.NotEqual(x.GetHashCode(), y.GetHashCode());
 		}
 	}
 }
