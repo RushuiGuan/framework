dc79a6315d09c0ab04ade88eb73c665d49e03dc7
rushuiguan <rguan@welton.com>
2023-03-14 12:45:52 -0400

    add hiLo property to EntityMap

--- src/Albatross.Repository.SqlServer/EntityMap.cs
+++ src/Albatross.Repository.SqlServer/EntityMap.cs
@@ -10,6 +10,7 @@ namespace Albatross.Repository.SqlServer {
 	public abstract class EntityMap<T> : IBuildEntityModel, IEntityMap<T> where T : class {
 		public virtual string TableName => typeof(T).Name;
 		public virtual bool Temporal => false;
+		public virtual string HiLo => $"{typeof(T).Name}HiLo";
 
 		public virtual void Map(EntityTypeBuilder<T> builder) {
 			builder.ToTable(TableName, b => b.IsTemporal(this.Temporal));
