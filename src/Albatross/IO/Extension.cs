f5464b49870cfb0f68621b754b707340a8f276b2
rushuiguan <rguan@welton.com>
2023-04-25 23:55:35 -0400

    add IO utility

---
+++ src/Albatross/IO/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Albatross.IO {
+	public static class Extension {
+		public static void EnsureDirectory(this string file) {
+			var directory = Path.GetDirectoryName(file);
+			if(directory != null) {
+				if (!Directory.Exists(directory)) {
+					Directory.CreateDirectory(directory);
+				}
+			}else {
+				throw new ArgumentException($"Path {file} is not valid");
+			}
+		}
+	}
+}
