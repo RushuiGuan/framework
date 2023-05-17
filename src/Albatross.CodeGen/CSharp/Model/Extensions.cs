8f816493b2ed12cfd86fb3dd6dc274c0a8fbb79b
rushuiguan <rguan@welton.com>
2023-05-17 12:08:52 -0400

    update codegen

--- src/Albatross.CodeGen/CSharp/Model/Extensions.cs
+++ src/Albatross.CodeGen/CSharp/Model/Extensions.cs
@@ -7,6 +7,6 @@ using System.IO;
 namespace Albatross.CodeGen.CSharp.Model {
 	public static class Extensions {
 		public static void WriteAttributes(this TextWriter writer, IEnumerable<MethodCall> attributes) =>
-			attributes.ForEach(args => writer.OpenSquareBracket().Code(args).CloseSquareBracket());
+			attributes.ForEach(args => writer.OpenSquareBracket().Code(args).CloseSquareBracket().WriteLine());
 	}
 }
