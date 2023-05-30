3769ba5732748d6514d4fddad5d31929dc7c12ae
rushuiguan <rguan@welton.com>
2023-05-30 09:58:32 -0400

    wip

---
+++ src/Albatross/Serialization/IDefaultSerializationOption.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Albatross.Serialization {
+	public interface IDefaultSerializationOption {
+		JsonSerializerOptions DefaultOptions { get; }
+	}
+}
