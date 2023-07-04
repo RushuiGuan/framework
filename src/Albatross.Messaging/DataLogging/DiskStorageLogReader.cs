1996bd14ffcd80213b4d1d1003cf8accf7a76af8
rushuiguan <rguan@welton.com>
2023-07-04 02:16:48 -0400

    change log reader so that a bad log won't crash the code

--- src/Albatross.Messaging/DataLogging/DiskStorageLogReader.cs
+++ src/Albatross.Messaging/DataLogging/DiskStorageLogReader.cs
@@ -10,10 +10,12 @@ namespace Albatross.Messaging.DataLogging {
 	public class DiskStorageLogReader :  ILogReader {
 		private readonly DiskStorageConfiguration config;
 		private readonly IMessageFactory messageFactory;
+		private readonly ILogger logger;
 
 		public DiskStorageLogReader(DiskStorageConfiguration config, IMessageFactory messageFactory, ILogger logger)  {
 			this.config = config;
 			this.messageFactory = messageFactory;
+			this.logger = logger;
 		}
 
 		public IEnumerable<LogEntry> ReadLast(TimeSpan span) {
@@ -33,10 +35,14 @@ namespace Albatross.Messaging.DataLogging {
 					while (!reader.EndOfStream) {
 						var line = reader.ReadLine();
 						if (line != null) {
-							if (LogEntry.TryParseLine(messageFactory, line, out var replay)) {
-								if (replay.TimeStamp >= cutOff) {
-									yield return replay;
-								}
+							LogEntry? replay = null;
+							try {
+								LogEntry.TryParseLine(messageFactory, line, out replay);
+							}catch(Exception ex) {
+								logger.LogError(ex, "Error parsing log entry: {line}", line);
+							}
+							if (replay?.TimeStamp >= cutOff) {
+								yield return replay;
 							}
 						}
 					}
