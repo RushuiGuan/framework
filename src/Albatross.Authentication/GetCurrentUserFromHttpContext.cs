fc3c4b854695e467eeaa040a0093f3c3457d3b11
rushuiguan <rguan@welton.com>
2023-07-25 08:51:21 -0400

    add usage logging

--- src/Albatross.Authentication/GetCurrentUserFromHttpContext.cs
+++ src/Albatross.Authentication/GetCurrentUserFromHttpContext.cs
@@ -4,6 +4,24 @@ using System;
 
 namespace Albatross.Authentication {
 	public class GetCurrentUserFromHttpContext : IGetCurrentUser {
+		public static string GetFromContext(HttpContext? context) {
+			string name = context?.User?.Identity?.Name;
+			if (string.IsNullOrEmpty(name)) {
+				name = context?.User?.FindFirst(ClaimType_Name)?.Value;
+			} else if (string.IsNullOrEmpty(name)) {
+				name = context?.User?.FindFirst(ClaimType_Preferred_Username)?.Value;
+			}
+			if (!string.IsNullOrEmpty(name)) {
+				int i = name.IndexOf('\\');
+				if (i >= 0) {
+					name = name.Substring(i + 1);
+				}
+				return name;
+			} else {
+				return "Anonymous";
+			}
+		}
+
 		IHttpContextAccessor httpContextAccessor;
 		public GetCurrentUserFromHttpContext(IHttpContextAccessor httpContextAccessor) {
 			this.httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
@@ -13,24 +31,6 @@ namespace Albatross.Authentication {
 		const string ClaimType_Preferred_Username = "preferred_username";
 		const string ClaimType_Name = "name";
 
-		public string Get() {
-			string name = httpContextAccessor.HttpContext?.User?.Identity?.Name;
-			if (string.IsNullOrEmpty(name)) {
-				name = httpContextAccessor.HttpContext?.User?.FindFirst(ClaimType_Name)?.Value;
-			}else if (string.IsNullOrEmpty(name)) {
-				name = httpContextAccessor.HttpContext?.User?.FindFirst(ClaimType_Preferred_Username)?.Value;
-			}
-			if (!string.IsNullOrEmpty(name)) {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-				int i = name.IndexOf('\\');
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-				if (i >= 0) {
-					name = name.Substring(i + 1);
-				}
-				return name;
-			} else {
-				return "Anonymous";
-			}
-		}
+		public string Get() => GetFromContext(httpContextAccessor.HttpContext);
 	}
 }
