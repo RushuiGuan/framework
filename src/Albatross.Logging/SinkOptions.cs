dfdae8e866b4fa1b44d608437c4973da3f22e67e
rushuiguan <rguan@welton.com>
2023-09-26 01:54:15 -0400

    create SlackSinkOptions so that the url can be retrieved using an environment variable

---
+++ src/Albatross.Logging/SinkOptions.cs
@@ -0,0 +1,16 @@
+﻿using Serilog.Sinks.Slack.Models;
+using System;
+
+namespace Albatross.Logging {
+	public class SinkOptions {
+		public static SlackSinkOptions SlackSink => slackSink.Value;
+
+		static readonly Lazy<SlackSinkOptions> slackSink = new Lazy<SlackSinkOptions>(() => new SlackSinkOptions {
+			WebHookUrl = Environment.GetEnvironmentVariable("SlackSinkWebHookUrl"),
+			BatchSizeLimit = 20,
+			ShowDefaultAttachments = false,
+			ShowPropertyAttachments = false,
+			ShowExceptionAttachments = true,
+		});
+	}
+}
