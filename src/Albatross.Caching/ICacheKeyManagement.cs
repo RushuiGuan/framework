28b0b0a2ef434c24768daa21f33282a4b90b991e
rushuiguan <rguan@welton.com>
2023-10-26 11:14:43 -0400

    add comment

--- src/Albatross.Caching/ICacheKeyManagement.cs
+++ src/Albatross.Caching/ICacheKeyManagement.cs
@@ -3,8 +3,18 @@ using System.Threading.Tasks;
 
 namespace Albatross.Caching {
 	public interface ICacheKeyManagement {
+		/// <summary>
+		/// key seeking is not a recommended operation in a caching setup.  should avoid if possible
+		/// </summary>
+		/// <param name="pattern"></param>
+		/// <returns></returns>
 		Task<IEnumerable<string>> FindKeys(string pattern);
 		Task FindAndRemoveKeys(string pattern);
+
+		/// <summary>
+		/// the remove operation should be quick and intentionally not async
+		/// </summary>
+		/// <param name="keys"></param>
 		void Remove(IEnumerable<string> keys);
 	}
 }
