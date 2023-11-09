1dc9bbbdda1070544735e5cef2038dec34344b24
rushuiguan <rguan@welton.com>
2023-11-08 22:21:48 -0500

    update serializaiton code

--- src/Albatross/Reflection/EnumText.cs
+++ src/Albatross/Reflection/EnumText.cs
@@ -7,24 +7,22 @@ namespace Albatross.Reflection {
 		public EnumTextAttribute(string text) {
 			Text = text;
 		}
-
 		public string Text { get; }
 	}
 
 	public static class EnumTextExtensions {
-		public static string GetEnumText<EnumType>(this EnumType value) where EnumType:struct 
-			=> value.GetEnumMemberAttribute<EnumType, EnumTextAttribute>().Text;
-
-		public static A GetEnumMemberAttribute<EnumType, A>(this EnumType enumValue) where A : Attribute where EnumType : struct {
+		public static A? GetEnumMemberAttribute<EnumType, A>(this EnumType enumValue) where A : Attribute where EnumType : struct {
 			var type = typeof(EnumType);
 			if (type.IsEnum) {
 				var value = enumValue.ToString();
 				var members = type.GetMember(value);
-				return members[0].GetCustomAttribute<A>()
-					?? throw new ArgumentException($"Enum field {enumValue} doesn't have attribute {typeof(A).Name}");
+				return members[0].GetCustomAttribute<A>();
 			} else {
 				throw new ArgumentException($"Type {type.FullName} is not an enum");
 			}
 		}
+		public static string GetEnumText<EnumType>(this EnumType value) where EnumType : struct
+			=> value.GetEnumMemberAttribute<EnumType, EnumTextAttribute>()?.Text 
+				?? throw new ArgumentException($"{value} is missing the {typeof(EnumTextAttribute)}");
 	}
 }
---
+++ tests/Albatross.Serialization.Test/TestJsonConverter.cs
@@ -0,0 +1,44 @@
+﻿using Albatross.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Albatross.Serialization.Test {
+	[JsonConverter(typeof(JsonEnumTextConverter<MyEnum>))]
+	public enum MyEnum {
+		[EnumText("wild cow")]
+		Cow,
+
+		[EnumText("small pig")]
+		Pig
+	}
+
+	[JsonConverter(typeof(JsonStringEnumConverter))]
+	public enum HisEnum {
+		A, 
+		B
+	}
+
+	public class TestJsonConverter {
+		[Theory]
+		[InlineData(MyEnum.Cow, "\"wild cow\"")]
+		[InlineData(MyEnum.Pig, "\"small pig\"")]
+		public void TestSerialization(MyEnum value, string expected) {
+			var text = JsonSerializer.Serialize(value);
+			Assert.Equal(expected, text);
+		}
+
+		[Theory]
+		[InlineData(MyEnum.Cow, "\"wild cow\"")]
+		[InlineData(MyEnum.Pig, "\"small pig\"")]
+		public void TestDeSerialization(MyEnum expected, string text) {
+			var result = JsonSerializer.Deserialize<MyEnum>(text);
+			Assert.Equal(expected, result);
+		}
+
+		[Fact]
+		public void Test() {
+			JsonSerializer.Deserialize<HisEnum>("\"\"");
+		}
+	}
+}
