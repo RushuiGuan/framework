4fee66c737b880deac1a49c6e24b609fd69bb89d
rushuiguan <rguan@welton.com>
2023-11-15 11:52:14 -0500

    add testing for cache reset

---
+++ tests/Albatross.Caching.Test/TestCacheKeyReset.cs
@@ -0,0 +1,39 @@
+﻿using Albatross.Hosting.Test;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Xunit;
+using System.Collections.Generic;
+using Albatross.Caching.Test.CacheMgmt;
+
+namespace Albatross.Caching.Test {
+	public class TestCacheKeyReset {
+		[Theory]
+		[InlineData(MemCacheHost.HostType)]
+		[InlineData(RedisCacheHost.HostType)]
+		public async Task TestResetOperation(string hostType) {
+			using var host = hostType.GetTestHost();
+			using var scope = host.Create();
+			var keyMgmt = scope.Get<ICacheKeyManagement>();
+			var cache = scope.ServiceProvider.GetRequiredService<StringKeyCacheMgmt>();
+			keyMgmt.Remove("*");
+
+			var keys = new List<string>();
+			for (int x = 0; x < 10; x++) {
+				keys.Add(x.ToString());
+				await cache.PutAsync(x.ToString(), new MyData());
+			}
+
+			// base verification that all keys are created
+			var allKeys = keyMgmt.FindKeys("*");
+			foreach (var item in keys) {
+				Assert.Contains(cache.CreateKey(item), allKeys);
+			}
+			// reset the key
+			cache.Reset();
+			allKeys = keyMgmt.FindKeys("*");
+			foreach (var item in keys) {
+				Assert.DoesNotContain(cache.CreateKey(item), allKeys);
+			}
+		}
+	}
+}
