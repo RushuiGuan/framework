f2ec0dcb86985a100f310263734229c16a4410f2
Rushui <rguan@welton.com>
2023-11-25 14:04:52 -0500

    working on packing script

--- authentication/pack.ps1
+++ authentication/pack.ps1
@@ -2,27 +2,8 @@ $InformationPreference = "Continue";
 $ErrorActionPreference = "Stop";
 . ..\scripts.ps1;
 
-if(Test-GitDiff){
-
-}
-
-$install = "$PSScriptRoot\artifacts";
-
-write-Information "Install path: $install";
-
 $projects = @(
 	"albatross.authentication"
 );
 
-foreach($item in $projects){
-	if(Test-Path "$install/$item" -type Container){
-		Write-Information "Deleting $item";
- 		Get-ChildItem $install\$item | Remove-Item -Recurse -Force;
-	}
-}
-
-dotnet restore  $PSScriptRoot
-foreach($project in $projects){
-	"Building $project";
-	dotnet pack $PSScriptRoot\$project\$project.csproj --output $install\$project --configuration release --no-restore
-}
+Run-Pack $projects $PSScriptRoot;
