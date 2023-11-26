1c7387b60325c30f639ce7157755fdfaaaae8a87
Rushui <rguan@welton.com>
2023-11-25 23:16:06 -0500

    update logging

---
+++ logging/pack.ps1
@@ -0,0 +1,18 @@
+param(
+	[switch]
+	[bool]$prod
+)
+$InformationPreference = "Continue";
+$ErrorActionPreference = "Stop";
+. ..\scripts\pack.ps1;
+
+$projects = @(
+	"Albatross.Logging"
+);
+
+Run-Pack -projects $projects `
+	-directory $PSScriptRoot `
+	-localSymbolServer $env:LocalSymbolServer `
+	-remoteSymbolServer $env:RemoteSymbolServer `
+	-nugetSource $env:DefaultNugetSource `
+	-prod:$prod
