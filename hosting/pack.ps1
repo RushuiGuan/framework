8e67395a83381cf56d8889972ec1ed5cea55badc
Rushui <rguan@welton.com>
2023-11-25 23:42:53 -0500

    fix hosting

---
+++ hosting/pack.ps1
@@ -0,0 +1,20 @@
+param(
+	[switch]
+	[bool]$prod
+)
+$InformationPreference = "Continue";
+$ErrorActionPreference = "Stop";
+. ..\scripts\pack.ps1;
+
+$projects = @(
+	"Albatross.Hosting",
+	"Albatross.Hosting.Test",
+	"Albatross.Hosting.Utility"
+);
+
+Run-Pack -projects $projects `
+	-directory $PSScriptRoot `
+	-localSymbolServer $env:LocalSymbolServer `
+	-remoteSymbolServer $env:RemoteSymbolServer `
+	-nugetSource $env:DefaultNugetSource `
+	-prod:$prod
