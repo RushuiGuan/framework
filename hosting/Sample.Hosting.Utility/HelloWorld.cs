9645da7693663d5c6335c257d9d0e208f793503a
rushuiguan <rguan@welton.com>
2023-12-04 10:08:35 -0500

    add sample code for utility

--- hosting/Sample.Hosting.Utility/HelloWorld.cs
+++ hosting/Sample.Hosting.Utility/HelloWorld.cs
@@ -1,6 +1,7 @@
 ﻿using Albatross.Hosting.Utility;
 using CommandLine;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace Sample.Hosting.Utility {
@@ -12,14 +13,15 @@ namespace Sample.Hosting.Utility {
 		}
 
 		/// <summary>
-		/// RunUtility is a method by convention
+		/// RunUtility is a method by convention.  The signature should always have a return type of Task&lt;int&gt;.  The parameters of the method can be any dependency 
+		/// injected objects.  The parameters are created by DI container using a scoped lifetime.
 		/// </summary>
 		/// <param name="logger"></param>
 		/// <returns>The return value of this method will set the exit code of the utility.  Normally return code of 0 indicates successful execution and another other values 
 		/// are treated as an error.  However, return code is an old concept, best practise is to throw an exception for errors.
 		/// to throw an exception </returns>
 		public  Task<int> RunUtility(ILogger<HelloWorld> logger) {
-			logger.LogInformation("Hello world");
+			logger.LogInformation("Hello world at {datetime:yyyy-MM-dd hh:mm:ss fff}", DateTime.Now);
 			return Task.FromResult(0);
 		}
 	}
