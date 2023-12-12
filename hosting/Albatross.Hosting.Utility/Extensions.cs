da8c4d07570d41ffc4224d8257a850ca609595a0
rushuiguan <rguan@welton.com>
2023-12-12 10:54:04 -0500

    Update project version from 6.4.2 to 6.4.3

--- hosting/Albatross.Hosting.Utility/Extensions.cs
+++ hosting/Albatross.Hosting.Utility/Extensions.cs
@@ -32,7 +32,7 @@ namespace Albatross.Hosting.Utility {
 		public static async Task<int> Run(this Parser parser, string[] args, params Assembly[] assemblies) {
 			Dictionary<Type, Type> dict = new Dictionary<Type, Type>();
 			foreach (var asm in assemblies) {
-				foreach(var type in asm.GetTypes()) {
+				foreach (var type in asm.GetTypes()) {
 					if (type.TryGetClosedGenericType(typeof(IUtility<>), out var genericType)) {
 						Type optionType = genericType.GetGenericArguments().First();
 						dict.Add(optionType, type);
@@ -40,27 +40,8 @@ namespace Albatross.Hosting.Utility {
 				}
 			}
 			dict.Add(typeof(ShowEnvironmentOption), typeof(ShowEnvironment));
-			ParserResult<Object> parserResult = parser.ParseArguments(args, dict.Keys.ToArray());
+			ParserResult<Object> parserResult = parser.ParseArguments(args, dict.Keys.OrderBy(x => x.GetCustomAttribute<VerbAttribute>()?.Name).ToArray());
 			return await parserResult.MapResult<object, Task<int>>(async opt => await RunAsync(opt, dict), err => Task.FromResult(1));
-		}
-
-		public static EntityType ReadInput<EntityType>(this string file) {
-			using var reader = new StreamReader(file);
-			string text = reader.ReadToEnd();
-			var result = JsonSerializer.Deserialize<EntityType>(text, new JsonSerializerOptions {
-				DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-				WriteIndented = true
-			});
-			if(result == null) {
-				throw new InvalidDataException($"File {file} cannot be deserialized into an object of {typeof(EntityType).Name}");
-			} else {
-				return result;
-			}
-		}
-		public static string ReadInput(this string file) {
-			using var reader = new StreamReader(file);
-			return reader.ReadToEnd();
 		}
 	}
 }
--- hosting/build.ps1
+++ hosting/build.ps1
@@ -1,24 +1,29 @@
+param(
+	[switch]
+	[bool]$alias
+)
 $InformationPreference = "Continue";
 $install = $env:InstallDirectory;
 
+if(-not $alias) {
+	$projects = @(
+		"sample.hosting.utility"
+		,"sample.hosting.webapi"
+		,"sample.hosting.daemon"
+	);
 
-$projects = @(
-	"sample.hosting.utility"
-	,"sample.hosting.webapi"
-	,"sample.hosting.daemon"
-);
-
-foreach($item in $projects){
-	if(Test-Path "$install/$item" -type Container){
-		Write-Information "Deleting $item";
- 		Get-ChildItem $install\$item | Remove-Item -Recurse -Force;
+	foreach($item in $projects){
+		if(Test-Path "$install/$item" -type Container){
+			Write-Information "Deleting $item";
+ 			Get-ChildItem $install\$item | Remove-Item -Recurse -Force;
+		}
 	}
-}
 
-dotnet restore $PSScriptRoot --interactive;
-foreach($project in $projects){
-	"Building $project";
-	dotnet publish $PSScriptRoot\$project\$project.csproj -o $install\$project -c debug --interactive;
+	dotnet restore $PSScriptRoot --interactive;
+	foreach($project in $projects){
+		"Building $project";
+		dotnet publish $PSScriptRoot\$project\$project.csproj -o $install\$project -c debug --interactive;
+	}
 }
 
 set-alias -name sample-hosting -Value $env:InstallDirectory\sample.hosting.utility\sample.hosting.utility.exe
