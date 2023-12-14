aa6dc3a9986a788d5ad0cb561953ff6e366f7290
rushuiguan <rguan@welton.com>
2023-12-13 19:09:54 -0500

    add dateonly conversion

---
+++ dates/Albatross.Dates/DateOnlyExtensions.cs
@@ -0,0 +1,23 @@
+﻿#if NET6_0_OR_GREATER
+using System;
+
+namespace Albatross.Dates {
+	public static class DateOnlyExtensions {
+		public static DateOnly ToDateOnly(this DateTime? dateTime, DateOnly fallbackValue) {
+			if(dateTime.HasValue) {
+				return DateOnly.FromDateTime(dateTime.Value);
+			} else {
+				return fallbackValue;
+			}
+		}
+
+		public static DateOnly? ToDateOnly(this DateTime? dateTime){
+			if(dateTime.HasValue) {
+				return DateOnly.FromDateTime(dateTime.Value);
+			} else {
+				return null;
+			}
+		}
+	}
+}
+#endif
