febde55cbfe094e73321b8d759d6ba70710c91df
rushuiguan <rguan@welton.com>
2024-02-29 03:20:09 -0500

    setup efcore caching mechanism

--- caching/Albatross.Caching/ICachedObject.cs
+++ caching/Albatross.Caching/ICachedObject.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Albatross.Caching {
-	public class CachedObject<T, K> : ICachedObject where T : ICacheManagement<K> {
-		public Func<object> GetCacheKey => throw new NotImplementedException();
-
-		public Type CacheMgmtType => throw new NotImplementedException();
-	}
-
-	public interface ICachedObject<Key> {
-		Func<Key> GetCacheKey{get; }
-		Type CacheMgmtType{get; }
+﻿namespace Albatross.Caching {
+	public interface ICachedObject {
+		void Invalidate(CacheEvictionService cacheEvictionService);
 	}
 }
