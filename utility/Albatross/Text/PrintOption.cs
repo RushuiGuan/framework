8aa05c1038cee2d9d00dc0384651e29dca16fac4
rushuiguan <rguan@welton.com>
2024-03-01 01:08:41 -0500

    enhance text printing capability

--- utility/Albatross/Text/PrintOption.cs
+++ utility/Albatross/Text/PrintOption.cs
@@ -3,13 +3,10 @@ using System.Threading.Tasks;
 
 namespace Albatross.Text {
 	public record class PrintOption {
-		public PrintOption(params string[] properties) {
-			this.Properties = properties;
-		}
 		public delegate Task<string> FormatValueDelegate(object? entity, string property, object? value);
-		public char ColumnHeaderLineCharacter { get; init; } = '-';
-		public FormatValueDelegate FormatValue { get; init; } = DefaultFormatValue;
-		public string[] Properties { get; init; }
+		public char ColumnHeaderLineCharacter { get; set; } = '-';
+		public FormatValueDelegate FormatValue { get; set; } = DefaultFormatValue;
+		public string[] Properties { get; set; } = Array.Empty<string>();
 
 		public static Task<string> DefaultFormatValue(object? _, string property, object? value) {
 			if (value is DateTime dateTime) {
@@ -22,19 +19,5 @@ namespace Albatross.Text {
 				return Task.FromResult(Convert.ToString(value) ?? string.Empty);
 			}
 		}
-	}
-
-	public record class PrintPropertiesOption : PrintOption {
-		public Func<int, string?>? GetColumnHeader { get; init; }
-		public Func<string, string> GetRowHeader { get; init; } = args => args;
-		public bool HasColumnHeaderLine => GetColumnHeader != null;
-
-		public PrintPropertiesOption(params string[] properties) : base(properties) { }
-	}
-
-	public record class PrintTableOption : PrintOption {
-		public bool PrintHeader { get; set; } = true;
-		public Func<string, string> GetColumnHeader { get; init; } = args => args;
-		public PrintTableOption(params string[] properties) : base(properties) { }
 	}
 }
---
+++ utility/Albatross/Text/PrintTableOption.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Albatross.Text {
+	public record class PrintTableOption : PrintOption {
+		public bool PrintHeader { get; set; } = true;
+		public Func<string, string> GetColumnHeader { get; set; } = args => args;
+	}
+}
