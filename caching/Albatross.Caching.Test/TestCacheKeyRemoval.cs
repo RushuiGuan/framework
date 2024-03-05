0e3a2cbc862245803ee1f6bf65e25341e067f355
rushuiguan <rguan@welton.com>
2024-03-05 01:18:10 -0500

    fix remove cache implementation

---
+++ caching/Albatross.Caching.Test/TestCacheKeyRemoval.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Albatross.Caching.Test {
+	public class TestCacheKeyRemoval {
+		[Fact]
+		public void TestRemoveSelfAndChildren() {
+			var mock = new Mock<ICacheKeyManagement>();
+			mock.Setup(x => x.FindKeys(It.IsAny<string>()))
+				.Returns(new string[] { "test:1:1:1", "test:2:1" });
+			var keyMgmt = mock.Object;
+			var keys = new ICacheKey[] {
+				new CacheKey("test", "1"),
+				new CacheKey("test", "1"),
+				new CacheKey("test", "2")
+			};
+			keyMgmt.RemoveSelfAndChildren(keys);
+			mock.Verify(x => x.FindKeys("test:1:*"), Times.Once);
+			mock.Verify(x => x.FindKeys("test:2:*"), Times.Once);
+			mock.Verify(x => x.Remove(new string[] { "test:1:1:1", "test:2:1" }), Times.Once);
+			mock.VerifyNoOtherCalls();
+		}
+
+		[Fact]
+		public void TestResetCach() {
+			var mock = new Mock<ICacheKeyManagement>();
+			mock.Setup(x => x.FindKeys(It.IsAny<string>()))
+				.Returns(new string[] { "test:1:1:1", "test:2:1" });
+			var keyMgmt = mock.Object;
+			var keys = new ICacheKey[] {
+				new CacheKey("test", "1"),
+				new CacheKey("test", "1"),
+				new CacheKey("test", "2"),
+				new CacheKey("xx", "2")
+			};
+			keyMgmt.Reset(keys);
+			mock.Verify(x => x.FindKeys("test:*"), Times.Once);
+			mock.Verify(x => x.FindKeys("xx:*"), Times.Once);
+			mock.Verify(x => x.Remove(new string[] { "test:1:1:1", "test:2:1" }), Times.Once);
+			mock.VerifyNoOtherCalls();
+		}
+	}
+}
--- caching/Albatross.Caching/Extension.cs
+++ caching/Albatross.Caching/Extension.cs
@@ -41,9 +41,13 @@ namespace Albatross.Caching {
 			keyMgmt.Remove(set.ToArray());
 		}
 		public static void RemoveSelfAndChildren(this ICacheKeyManagement keyMgmt, params ICacheKey[] keys) {
+			var wildCardKeys = new HashSet<string>();
 			var set = new HashSet<string>();
 			foreach (var key in keys) {
-				set.AddRange(keyMgmt.FindKeys(key.WildCardKey));
+				if(!wildCardKeys.Contains(key.WildCardKey)) {
+					wildCardKeys.Add(key.WildCardKey);
+					set.AddRange(keyMgmt.FindKeys(key.WildCardKey));
+				}
 			}
 			keyMgmt.Remove(set.ToArray());
 		}
@@ -52,9 +56,13 @@ namespace Albatross.Caching {
 			keyMgmt.RemoveSelfAndChildren(keys);
 		}
 		public static void Reset(this ICacheKeyManagement keyMgmt, params ICacheKey[] keys) {
+			var wildCardKeys = new HashSet<string>();
 			var set = new HashSet<string>();
 			foreach (var key in keys) {
-				set.AddRange(keyMgmt.FindKeys(key.ResetKey));
+				if (!wildCardKeys.Contains(key.ResetKey)) {
+					wildCardKeys.Add(key.ResetKey);
+					set.AddRange(keyMgmt.FindKeys(key.ResetKey));
+				}
 			}
 			keyMgmt.Remove(set.ToArray());
 		}
