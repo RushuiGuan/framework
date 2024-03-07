a3674eae9d2fc34b2182ad01d5dd66d5a9b7a423
rushuiguan <rguan@welton.com>
2024-03-07 11:41:36 -0500

    wip

--- efcore/Albatross.EFCore.Audit/AuditChangeEventHandler.cs
+++ efcore/Albatross.EFCore.Audit/AuditChangeEventHandler.cs
@@ -4,25 +4,24 @@ using System;
 using System.Threading.Tasks;
 
 namespace Albatross.EFCore.Audit {
-	public class AuditDbSessionEventHandler : IDbChangeEventHandler {
-		public string User { get; }
-
+	public class AuditChangeEventHandler : IDbChangeEventHandler {
+		private readonly IGetCurrentUser getCurrentUser;
 		public bool HasPostSaveOperation => false;
 		public Task PostSave() => Task.CompletedTask;
 
-		public AuditDbSessionEventHandler(IGetCurrentUser getCurrentUser) {
-			User = getCurrentUser.Get();
+		public AuditChangeEventHandler(IGetCurrentUser getCurrentUser) {
+			this.getCurrentUser = getCurrentUser;
 		}
 		public void OnAddedEntry(EntityEntry entry) {
-			if (entry.Entity is ICreatedBy audit1) { audit1.CreatedBy = this.User; }
-			if (entry.Entity is IModifiedBy audit2) { audit2.ModifiedBy = this.User; }
+			if (entry.Entity is ICreatedBy audit1) { audit1.CreatedBy = this.getCurrentUser.Get(); }
+			if (entry.Entity is IModifiedBy audit2) { audit2.ModifiedBy = this.getCurrentUser.Get(); }
 			var utcNow = DateTime.UtcNow;
 			if (entry.Entity is ICreatedUtc audit3) { audit3.CreatedUtc = utcNow; }
 			if (entry.Entity is IModifiedUtc audit4) { audit4.ModifiedUtc = utcNow; }
 		}
 
 		public void OnModifiedEntry(EntityEntry entry) {
-			if (entry.Entity is IModifiedBy audit1) { audit1.ModifiedBy = this.User; }
+			if (entry.Entity is IModifiedBy audit1) { audit1.ModifiedBy = this.getCurrentUser.Get(); }
 			if (entry.Entity is IModifiedUtc audit2) { audit2.ModifiedUtc = DateTime.UtcNow; }
 		}
 		public void OnDeletedEntry(EntityEntry entry) { }
--- efcore/Albatross.EFCore.Audit/Extensions.cs
+++ efcore/Albatross.EFCore.Audit/Extensions.cs
@@ -3,7 +3,7 @@
 namespace Albatross.EFCore.Audit {
 	public static class Extensions {
 		public static IServiceCollection AddAuditEventHandlers(this IServiceCollection services) {
-			services.AddScoped<IDbChangeEventHandler, AuditDbSessionEventHandler>();
+			services.AddScoped<IDbChangeEventHandler, AuditChangeEventHandler>();
 			return services;
 		}
 	}
