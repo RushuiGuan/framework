d2475ab4403eb4db3fd9135044513ddc266e9727
rushuiguan <rguan@welton.com>
2024-03-11 08:12:59 -0400

    introduce object state

--- caching/Albatross.Caching/ICachedObject.cs
+++ caching/Albatross.Caching/ICachedObject.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace Albatross.Caching {
+﻿namespace Albatross.Caching {
+	public enum ObjectState {
+		Added = 0, Modified = 1, Deleted = 2,
+	}
 	public interface ICachedObject {
-		ICacheKey CreateCacheKey(int state, object? originalValues);
+		ICacheKey CreateCacheKey(ObjectState state, object? originalValues);
 	}
 }
