2adad2515f9833760f965bdca761b7e10383eb47
rushuiguan <rguan@welton.com>
2024-03-11 08:28:26 -0400

    CreateCacheKeys needs to return multiple items

--- caching/Albatross.Caching/ICachedObject.cs
+++ caching/Albatross.Caching/ICachedObject.cs
@@ -1,8 +1,10 @@
-﻿namespace Albatross.Caching {
+﻿using System.Collections.Generic;
+
+namespace Albatross.Caching {
 	public enum ObjectState {
 		Added = 0, Modified = 1, Deleted = 2,
 	}
 	public interface ICachedObject {
-		ICacheKey CreateCacheKey(ObjectState state, object? originalValues);
+		IEnumerable<ICacheKey> CreateCacheKeys(ObjectState state, object? originalValues);
 	}
 }
