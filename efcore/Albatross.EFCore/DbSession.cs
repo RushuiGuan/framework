1d21d4a7bc715c6bb7169d0238a0606eac4adce3
rushuiguan <rguan@welton.com>
2024-03-20 21:41:48 -0400

    remove reference to collection

--- efcore/Albatross.EFCore/DbSession.cs
+++ efcore/Albatross.EFCore/DbSession.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Albatross.Text;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.Data;
 using System.Reflection;
