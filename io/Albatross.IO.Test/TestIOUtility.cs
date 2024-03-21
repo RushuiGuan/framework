907fe5801cc7dd1e9e671005a8b2e8ada38909c6
rushuiguan <rguan@welton.com>
2024-03-20 22:51:55 -0400

    new project

---
+++ io/Albatross.IO.Test/TestIOUtility.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+using Albatross.IO;
+
+namespace Albatross.Test.IO {
+	public class TestIOUtility {
+
+		[Fact]
+		public void TestEnsureDirectory() {
+			@"c:\temp\test\me\like".EnsureDirectory();
+		}
+	}
+}
