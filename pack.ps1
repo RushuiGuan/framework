d150c60d39cbc9a1fb3d9b04229925145aa8ec61
rushuiguan <rguan@welton.com>
2024-03-25 14:50:20 -0400

    update packing script

--- pack.ps1
+++ pack.ps1
@@ -2,14 +2,50 @@ param(
 	[Parameter(Position=0)]
 	[string]$project,
 	[switch]
-	[bool]$prod
+	[bool]$prod,
+	[switch]
+	[bool]$force
 )
 $InformationPreference = "Continue";
 $ErrorActionPreference = "Stop";
 . $PSScriptRoot\scripts\pack.ps1;
 
 $projects = @(
-	"Albatross.Math"
+	"authentication\Albatross.Authentication",
+	"authentication\Albatross.Authentication.AspNetCore",
+	"authentication\Albatross.Authentication.Windows",
+	"caching\Albatross.Caching",
+	"caching\Albatross.Caching.Controllers",
+	"caching\Albatross.Caching.MemCache",
+	"caching\Albatross.Caching.Redis",
+	"caching\Albatross.EFCore.AutoCacheEviction",
+	"codegen\Albatross.CodeGen",
+	"codegen\Albatross.CodeGen.CSharp",
+	"codegen\Albatross.CodeGen.TypeScript",
+	"codegen\Albatross.CodeGen.WebClient",
+	"collections\Albatross.Collections",
+	"config\Albatross.Config",
+	"dates\Albatross.Dates",
+	"efcore\Albatross.DateLevel",
+	"efcore\Albatross.EFCore",
+	"efcore\Albatross.EFCore.Audit",
+	"efcore\Albatross.EFCore.ChangeReporting",
+	"efcore\Albatross.EFCore.PostgreSQL",
+	"efcore\Albatross.EFCore.SqlServer",
+	"excel\Albatross.Excel",
+	"excel\Albatross.Hosting.Excel",
+	"hosting\Albatross.Hosting",
+	"hosting\Albatross.Hosting.Test",
+	"hosting\Albatross.Hosting.Utility",
+	"io\Albatross.IO",
+	"logging\Albatross.Logging",
+	"math\Albatross.Math",
+	"messaging\Albatross.Messaging",
+	"reflection\Albatross.Reflection",
+	"serialization\Albatross.Serialization",
+	"text\Albatross.Text",
+	"threading\Albatross.Threading",
+	"webclient\Albatross.WebClient"
 );
 
 if(-not [string]::IsNullOrEmpty($project)){
@@ -19,4 +55,5 @@ if(-not [string]::IsNullOrEmpty($project)){
 Run-Pack -projects $projects `
 	-directory $PSScriptRoot `
 	-nugetSource $env:DefaultNugetSource `
-	-prod:$prod
+	-prod:$prod `
+	-force:$force
--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -11,34 +11,33 @@ function Run-Pack {
 		[Parameter(Mandatory)]
 		[string[]]$projects,
 		[string]$nugetSource,
-		[string]$remoteSymbolServer,
 		[switch]
-		[bool]$prod
+		[bool]$prod,
+		[switch]
+		[bool]$force
 	)
 	process {
-		if (Test-GitDiff $directory) {
+		if ((-not $force) -and (Test-GitDiff $directory)) {
 			Write-Error "Please commit your changes before packing";
 		}
+		dotnet restore $directory --verbosity minimal
+		
 		$artifacts = "$directory\artifacts";
-		$name = (Get-Item $directory).Name.ToLower();
+		Write-Information "Cleaning artifacts directory: $artifacts"
+		Get-ChildItem $artifacts | foreach-object { Remove-Item $_.FullName; }
+
 		$versionFile = "$directory\Directory.Build.props";
 		if (-not (Test-Path $versionFile -type Leaf)) {
 			Write-Error "Missing Directory.Build.props file";
-		}
-		foreach ($project in $projects) {
-			Write-Information "Deleting existing artifacts for $project";
-			Set-Directory "$artifacts\$project";
-			Get-ChildItem $artifacts\$project | Remove-Item -Recurse -Force;
 		}
 		$currentVersion = Get-ProjectVersion $versionFile;
 		$version = Get-NewProjectVersion $versionFile -prod:$prod;
 		Set-ProjectVersion -csproj $versionFile -version $version;
 		Write-Information "Project version updated to: $version";
-		dotnet restore $directory --interactive;
 		foreach ($project in $projects) {
 			Write-Information "Building $project";
-			dotnet pack $directory\$project\$project.csproj `
-				--output $artifacts\$project `
+			dotnet pack $directory\$project `
+				--output $artifacts `
 				--configuration release `
 				--no-restore
 		}
@@ -46,27 +45,17 @@ function Run-Pack {
 		git checkout $versionFile;
 
 		$hasNugetPush = $false;
-		foreach ($project in $projects) {
-			if ($nugetSource) { 
-				get-item $artifacts\$project\*.nupkg | foreach-Object {
-					nuget push $_.FullName -source $nugetSource -apiKey az
-					if ($exitcode -ne 0){
-					#	Write-Error "Error push nuget package $($_.Name)";
-					}
-					$hasNugetPush = $true;
+		if ($nugetSource) { 
+			get-item $artifacts\*.nupkg | foreach-Object {
+				nuget push $_.FullName -source $nugetSource -apiKey az
+				if ($exitcode -ne 0){
+				#	Write-Error "Error push nuget package $($_.Name)";
 				}
+				$hasNugetPush = $true;
 			}
-			try {
-				if ($remoteSymbolServer -and (Test-Path $remoteSymbolServer -type Container)) {
-					$path = (Set-Directory $remoteSymbolServer)
-					write-Information "copy remote symbol for $project => $path";
-					copy-item -Path $artifacts\$project\*.snupkg -Destination $path -Verbose;
-				}
-			}
-			catch {}
 		}
 		if ($hasNugetPush -and $prod) {
-			$tag = "$name-$currentVersion";
+			$tag = "prod-$currentVersion";
 			Write-Information "Tagging: $tag";
 			git tag $tag;
 		}
