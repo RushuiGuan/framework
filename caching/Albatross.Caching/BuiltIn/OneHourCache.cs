5287d2feaeaad297bc785597dd852112f3ce8f99
rushuiguan <rguan@welton.com>
2024-04-03 15:08:27 -0400

    add caching

---
+++ caching/Albatross.Caching/BuiltIn/OneHourCache.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly.Caching;
+using Polly.Registry;
+using System;
+
+namespace Albatross.Caching.BuiltIn {
+	public class OneHourCache<CacheFormat, KeyFormat> : Cache<CacheFormat, KeyFormat> where KeyFormat : ICacheKey {
+		public OneHourCache(ILogger<OneHourCache<CacheFormat, KeyFormat>> logger,
+				IPolicyRegistry<string> registry,
+				ICacheProviderAdapter cacheProviderAdapter) : base(logger, registry, cacheProviderAdapter) {
+		}
+
+		public override ITtlStrategy TtlStrategy => new RelativeTtl(TimeSpan.FromHours(1));
+	}
+}
---
+++ math/Albatross.Math.Test/TestToMaxBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Albatross.Math;
+using Xunit;
+
+namespace Albatross.Test.Math {
+	public class TestToMaxBase {
+		[Fact]
+		public void Run() {
+			var set = new HashSet<string>();
+			long tick = DateTime.Now.Ticks;
+			for (int i = 0; i < 100000000; i++) {
+				var text= tick.ToMaxBase();
+				if(set.Contains(text)) {
+					throw new Exception("Duplicate value");
+				}else{
+					set.Add(text);
+				}
+				tick++;
+			}
+		}
+
+		[Theory]
+		[InlineData(395916494494100, "1oQKZr3bB")]
+		public void GetMaxBase(long value, string expected) {
+			Assert.Equal(expected, value.ToMaxBase());
+		}
+	}
+}
