c47214f280fbcab903cf430acaf3d5d9fbb006ca
rushuiguan <rguan@welton.com>
2024-04-05 14:53:19 -0400

    add an easy way to return back errors without throwing an exception

--- hosting/Albatross.Hosting/Extensions.cs
+++ hosting/Albatross.Hosting/Extensions.cs
@@ -1,7 +1,24 @@
-﻿namespace Albatross.Hosting {
+﻿using System;
+using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
+
+namespace Albatross.Hosting {
 	public static class Extensions {
 		public static void SetCurrentDirectory() {
 			System.Environment.CurrentDirectory = System.IO.Path.GetDirectoryName(typeof(Extensions).Assembly.Location)!;
+		}
+
+		public static ErrorMessage ErrorMessage(this Exception err, ILogger logger, [CallerMemberName] string methodName = "") {
+			logger.LogWarning(err, methodName);
+			return new ErrorMessage {
+				Message = err.Message,
+				Type = err.GetType().FullName,
+			};
+		}
+
+		public static string ErrorTextMessage(this Exception err, ILogger logger, [CallerMemberName] string methodName = "") {
+			logger.LogWarning(err, methodName);
+			return err.Message;
 		}
 	}
 }
--- hosting/Sample.Hosting.WebApi/Controllers/ValuesController.cs
+++ hosting/Sample.Hosting.WebApi/Controllers/ValuesController.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Albatross.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Sample.Hosting.WebApi.Controllers {
 	[Route("api/[controller]")]
 	[ApiController]
 	public class ValuesController : ControllerBase {
+		private readonly ILogger logger;
+
+		public ValuesController(ILogger logger) {
+			this.logger = logger;
+		}
+
 		[HttpGet]
 		public string Get() {
 			return DateTime.UtcNow.ToString();
+		}
+
+
+		[HttpGet("error")]
+		public ActionResult<string> TestError(string msg) {
+			try {
+				// return DateTime.UtcNow.ToString();
+				throw new ArgumentException(msg);
+			} catch (ArgumentException err) {
+				return BadRequest(err.ErrorMessage(logger));
+			}
+		}
+
+		[HttpGet("error2")]
+		public ActionResult<string> TestError2(string msg) {
+			try {
+				// return DateTime.UtcNow.ToString();
+				throw new ArgumentException(msg);
+			} catch (ArgumentException err) {
+				return BadRequest(err.ErrorTextMessage(logger));
+			}
 		}
 	}
 }
