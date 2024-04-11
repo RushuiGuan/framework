fbf3557d639004bd2a17255eccdee6d9c7a7d26e
rushuiguan <rguan@welton.com>
2024-04-11 10:34:11 -0400

    rename the generic type

--- caching/Albatross.Caching/ICachedObject.cs
+++ caching/Albatross.Caching/ICachedObject.cs
@@ -4,7 +4,7 @@ namespace Albatross.Caching {
 	public enum ObjectState {
 		Added = 0, Modified = 1, Deleted = 2,
 	}
-	public interface ICachedObject<EntityEntryType, PropertyEntryType> {
-		IEnumerable<ICacheKey> CreateCacheKeys(ObjectState state, EntityEntryType entityEntry, IEnumerable<PropertyEntryType> changes);
+	public interface ICachedObject<Context, PropertyEntryType> {
+		IEnumerable<ICacheKey> CreateCacheKeys(ObjectState state, Context context, IEnumerable<PropertyEntryType> changes);
 	}
 }
