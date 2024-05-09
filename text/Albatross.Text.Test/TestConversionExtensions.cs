35f59e7c684f344577dac6fedb9897896a19244d
rushuiguan <rguan@welton.com>
2024-05-09 11:44:52 -0400

    add text to value conversion in text lib

---
+++ text/Albatross.Text.Test/TestConversionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Albatross.Text.Test {
+	public class TestConversionExtensions {
+		public static IEnumerable<object?[]> ConversionData => new List<object?[]> {
+			new object[] { "1", 1, typeof(int)},
+			new object?[] { null, 0, typeof(int)},
+
+			new object[] { "1", 1L, typeof(long)},
+			new object?[] { null, 0L, typeof(long)},
+
+			new object[] { "test", "test", typeof(string)},
+			new object[] { "", "", typeof(string)},
+			new object?[] { null, null, typeof(string)},
+
+			new object[] { "2024-05-01", new DateTime(2024, 05, 01), typeof(DateTime)},
+			new object[] { "", DateTime.MinValue, typeof(DateTime)},
+			new object?[] { null, DateTime.MinValue, typeof(DateTime)},
+
+			new object[] { "2024-05-01", new DateOnly(2024, 05, 01), typeof(DateOnly)},
+			new object[] { "", DateOnly.MinValue, typeof(DateOnly)},
+			new object?[] { null, DateOnly.MinValue, typeof(DateOnly)},
+
+			new object[] { "100", 100M, typeof(decimal)},
+			new object[] { "", 0M, typeof(decimal)},
+			new object?[] { null, 0M, typeof(decimal)},
+
+			new object[] { "100", 100.0, typeof(double)},
+			new object[] { "", 0.0, typeof(double)},
+			new object ?[] { null, 0.0, typeof(double) },
+
+			new object?[] { "", null, typeof(int?)},
+			new object?[] { null, null, typeof(int?)},
+			new object?[] { 1, 1, typeof(int?)},
+		};
+
+		[Theory]
+		[MemberData(nameof(ConversionData))]
+		public void TestValueConversion(string? text, object? expected, Type type) {
+			var actual = text.Convert(type);
+			Assert.Equal(expected, actual);
+		}
+	}
+}
---
+++ text/Albatross.Text/ConversionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Albatross.Reflection;
+
+namespace Albatross.Text {
+	public static class ConversionExtensions {
+		public static object? Convert(this string? text, Type type) {
+			if (string.IsNullOrEmpty(text)) {
+				if (type == typeof(string)) {
+					return text;
+				}else if (type.IsValueType) {
+					return Activator.CreateInstance(type);
+				} else {
+					return null;
+				}
+			}
+			switch (Type.GetTypeCode(type)) {
+				case TypeCode.Int32: return int.Parse(text);
+				case TypeCode.Int64: return long.Parse(text);
+				case TypeCode.String: return text;
+				case TypeCode.DateTime: return DateTime.Parse(text);
+				case TypeCode.Decimal: return decimal.Parse(text);
+				case TypeCode.Double: return double.Parse(text);
+				case TypeCode.Boolean: return bool.Parse(text);
+				case TypeCode.Char: return text[0];
+			};
+			if (type.IsEnum) {
+				return Enum.Parse(type, text, true);
+#if NET6_0_OR_GREATER
+			} else if (type == typeof(DateOnly)) {
+				return DateOnly.Parse(text);
+#endif
+			} else if (type.GetNullableValueType(out var valueType)) {
+				return Convert(text, valueType);
+			} else {
+				throw new NotSupportedException();
+			}
+		}
+
+		public static T Convert<T>(this IDictionary<string, string> dictionary) where T : new() {
+			var t = new T();
+			foreach (var keyValuePair in dictionary) {
+				var property = typeof(T).GetProperty(keyValuePair.Key);
+				if (property?.GetSetMethod() != null) {
+					var value = keyValuePair.Value.Convert(property.PropertyType);
+					property.SetValue(t, value);
+				}
+			}
+			return t;
+		}
+	}
+}
