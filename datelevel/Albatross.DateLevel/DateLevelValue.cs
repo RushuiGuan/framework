5640e1b5f38eec1c7f2ba683e7f078aa37b92130
rushuiguan <rguan@welton.com>
2024-06-10 14:01:23 -0400

    create new type DateLevelValue and bump version

---
+++ datelevel/Albatross.DateLevel/DateLevelValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Albatross.DateLevel {
+	public record class DateLevelValue<T> : IDateLevelEntity {
+		public DateLevelValue(T value, DateOnly startDate, DateOnly endDate) {
+			Value = value;
+			StartDate = startDate;
+			EndDate = endDate;
+		}
+
+		public T Value { get; }
+		public DateOnly StartDate { get; }
+		public DateOnly EndDate { get; }
+	}
+
+	public record class DateLevelValue<T, K> : IDateLevelEntity where K : IEquatable<K> {
+		public DateLevelValue(T value, K key, DateOnly startDate, DateOnly endDate) {
+			Value = value;
+			this.Key = key;
+			StartDate = startDate;
+			EndDate = endDate;
+		}
+
+		public T Value { get; }
+		public K Key { get; }
+		public DateOnly StartDate { get; }
+		public DateOnly EndDate { get; }
+	}
+}
