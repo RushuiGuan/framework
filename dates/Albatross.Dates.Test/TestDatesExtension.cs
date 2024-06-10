4f500fd01da9d9e2d4b780ee61628014f0c971ac
rushuiguan <rguan@welton.com>
2024-06-10 13:07:55 -0400

    fix dates testing

--- dates/Albatross.Dates.Test/TestDatesExtension.cs
+++ dates/Albatross.Dates.Test/TestDatesExtension.cs
@@ -1,22 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Albatross.Dates.Test {
 	public class TestDatesExtension {
-		[Theory]
-		[InlineData(null, "2021-12-06", "2021-12-06")]
-		[InlineData("2021-12-07", "2021-12-06", "2021-12-07")]
-		public void TestDateOnlyConversion(string? date, string fallback, string expected) {
-			DateTime? value = date == null ? null : DateTime.Parse(date);
-			var dateOnly = value.ToDateOnly(DateOnly.Parse(fallback));
-			Assert.Equal(expected, $"{dateOnly:yyyy-MM-dd}");
-		}
-
-
 		[Theory]
 		[InlineData(null, null)]
 		[InlineData("2021-12-07", "2021-12-07")]
@@ -25,16 +11,6 @@ namespace Albatross.Dates.Test {
 			var dateOnly = value.DateOnly();
 			Assert.Equal(expected, dateOnly.HasValue ? $"{dateOnly:yyyy-MM-dd}" : null);
 		}
-
-		[Theory]
-		[InlineData(null, "2021-12-06", "2021-12-06")]
-		[InlineData("2021-12-07", "2021-12-06", "2021-12-07")]
-		public void TestDateTimeConversion(string? date, string fallback, string expected) {
-			DateTime? value = date == null ? null : DateTime.Parse(date);
-			var dateOnly = value.ToDateOnly(DateOnly.Parse(fallback));
-			Assert.Equal(expected, $"{dateOnly:yyyy-MM-dd}");
-		}
-
 
 		[Theory]
 		[InlineData(null, null)]
