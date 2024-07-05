e6e07a816b9aa92ea3f1857c8b69fe1db82923fa
Rushui <rguan@welton.com>
2024-07-05 10:21:37 -0400

    wip

---
+++ codegen/Albatross.CodeGen.Utility/ApiControllerClassWalker.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+
+namespace Albatross.Messaging.CodeGen {
+	public class ApiControllerClassWalker: CSharpSyntaxWalker {
+		private readonly SemanticModel semanticModel;
+		public List<INamedTypeSymbol> Result { get; } = new List<INamedTypeSymbol>();
+
+		public ApiControllerClassWalker(SemanticModel semanticModel) {
+			this.semanticModel = semanticModel;
+		}
+	}
+}
---
+++ codegen/Albatross.CodeGen.Utility/GenerateTypeScriptProxy.cs
@@ -0,0 +1,36 @@
+﻿using Albatross.Hosting.Utility;
+using Albatross.Messaging.CodeGen;
+using CommandLine;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Albatross.CodeGen.Utility {
+	[Verb("typescript-proxy")]
+	public class GenerateTypeScriptProxyOption : BaseOption {
+		[Option('p', "project-file", Required = true)]
+		public string ProjectFile { get; set; } = string.Empty;
+	}
+	public class GenerateTypeScriptProxy : UtilityBase<GenerateTypeScriptProxyOption> {
+		public GenerateTypeScriptProxy(GenerateTypeScriptProxyOption option) : base(option) { }
+
+		public async Task<int> RunUtility(ILogger<GenerateTypeScriptProxy> logger) {
+			var workspace = MSBuildWorkspace.Create();
+			Project project = await workspace.OpenProjectAsync(Options.ProjectFile);
+			var compilation = await project.GetCompilationAsync();
+			var cadidates = new List<INamedTypeSymbol>();
+			if (compilation != null) {
+				foreach (var syntaxTree in compilation.SyntaxTrees) {
+					var semanticModel = compilation.GetSemanticModel(syntaxTree);
+					var walker = new ApiControllerClassWalker(semanticModel);
+					walker.Visit(syntaxTree.GetRoot());
+					cadidates.AddRange(walker.Result);
+				}
+			}
+
+			return 0;
+		}
+	}
+}
