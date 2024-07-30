2dea73a9b7fceeac5950cbe1380badfd1cfdfddb
rushuiguan <rguan@welton.com>
2024-07-30 01:12:52 -0400

    upgrade utility host

---
+++ hosting/Albatross.Hosting.Utility/CommandBuilder.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albatross.Hosting.Utility {
+	public class CommandBuilder {
+		Dictionary<Type, Type> dict = new Dictionary<Type, Type>();
+		public CommandBuilder AddUtility<TOption, TUtility>() where TUtility : IUtility<TOption>{
+			dict.Add(typeof(TOption), typeof(TUtility));
+			return this;
+		}
+		public Task<int> Run(Parser parser, string[] args) {
+			ParserResult<Object> parserResult = parser.ParseArguments(args, dict.Keys.ToArray());
+			return parserResult.MapResult<object, Task<int>>(async opt => await Extensions.RunAsync(opt, dict), err => Task.FromResult(1));
+		}
+	}
+}
--- hosting/Sample.Hosting.Utility/Program.cs
+++ hosting/Sample.Hosting.Utility/Program.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 namespace Sample.Hosting.Utility {
 	internal class Program {
 		static Task<int> Main(string[] args) {
-			return Parser.Default.Run(args, typeof(Program).Assembly);
+			// return Parser.Default.Run(args, typeof(Program).Assembly);
+			return new CommandBuilder()
+				.AddUtility<ShowEnvironmentOption, ShowEnvironment>()
+				.AddUtility<HelloWorldOption, HelloWorld>()
+				.Run(Parser.Default, args);
 		}
 	}
 }
