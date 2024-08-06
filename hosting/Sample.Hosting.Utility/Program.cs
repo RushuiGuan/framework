9c09c292cccdfeecbc9d36062febb8f0942f91ca
rushuiguan <rguan@welton.com>
2024-08-06 08:26:56 -0400

    revert old changes

--- hosting/Sample.Hosting.Utility/Program.cs
+++ hosting/Sample.Hosting.Utility/Program.cs
@@ -5,11 +5,7 @@ using System.Threading.Tasks;
 namespace Sample.Hosting.Utility {
 	internal class Program {
 		static Task<int> Main(string[] args) {
-			// return Parser.Default.Run(args, typeof(Program).Assembly);
-			return new CommandBuilder()
-				.AddUtility<ShowEnvironmentOption, ShowEnvironment>()
-				.AddUtility<HelloWorldOption, HelloWorld>()
-				.Run(Parser.Default, args);
+			return Parser.Default.Run(args, typeof(Program).Assembly);
 		}
 	}
 }
