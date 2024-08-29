b4a19e73ffa93a9cfad2f2c79aa86a0531565740
rushuiguan <rguan@welton.com>
2024-08-29 10:09:41 -0400

    reorg code analysis

--- codeanalysis/Albatross.CodeAnalysis.Test/TestClassBuilding.cs
+++ codeanalysis/Albatross.CodeAnalysis.Test/TestClassBuilding.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Albatross.CodeAnalysis.Syntax;
 using Xunit;
 
 namespace Albatross.CodeAnalysis.Test {
