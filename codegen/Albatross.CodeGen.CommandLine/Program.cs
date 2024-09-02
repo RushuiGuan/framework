d51058cd0595237bdb29cd35724b53987cff6e53
Rushui <rguan@welton.com>
2024-09-02 17:04:38 -0400

    wip

--- codegen/Albatross.CodeGen.CommandLine/Program.cs
+++ codegen/Albatross.CodeGen.CommandLine/Program.cs
@@ -2,13 +2,12 @@
 using Albatross.CommandLine;
 using Albatross.CodeGen.CommandLine;
 using System.Threading.Tasks;
-using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
 
 namespace Albatross.CodeGen.Tests.Utility {
 	internal class Program {
 		static async Task<int> Main(string[] args) {
-			var setup = new MySetup().AddCommandHandlers();
+			var setup = new MySetup().AddCommands();
 			var parser = setup.CommandBuilder.Build();
 			return await parser.InvokeAsync(args);
 		}
