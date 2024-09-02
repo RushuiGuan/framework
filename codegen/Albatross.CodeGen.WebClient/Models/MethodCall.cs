310fe7787cdafbf890b98d1bda2b1b776d2cbdc4
Rushui <rguan@welton.com>
2024-09-02 17:04:38 -0400

    wip

---
+++ codegen/Albatross.CodeGen.WebClient/Models/MethodCall.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albatross.CodeGen.WebClient.Models {
+	internal class MethodCall {
+	}
+}
