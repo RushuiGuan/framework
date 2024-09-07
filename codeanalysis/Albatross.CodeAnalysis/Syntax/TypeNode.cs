aceeb6bbbb5aaa7a5355b43cc7760c27196097fa
rushuiguan <rguan@welton.com>
2024-09-06 21:50:25 -0400

    wip

--- codeanalysis/Albatross.CodeAnalysis/Syntax/TypeNode.cs
+++ codeanalysis/Albatross.CodeAnalysis/Syntax/TypeNode.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Albatross.CodeAnalysis.Syntax {
 	public class TypeNode : NodeContainer {
 		public TypeNode(string name)
 			: base(SyntaxFactory.ParseTypeName(name)) { }
+
+		public TypeSyntax Type => (TypeSyntax)this.Node;
 	}
 }
