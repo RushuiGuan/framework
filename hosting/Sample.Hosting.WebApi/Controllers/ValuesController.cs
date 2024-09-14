732c459a8c6075347729694aa37779647390a6c5
Rushui <rguan@welton.com>
2024-09-14 15:22:58 -0400

    wip

--- hosting/Sample.Hosting.WebApi/Controllers/ValuesController.cs
+++ hosting/Sample.Hosting.WebApi/Controllers/ValuesController.cs
@@ -18,6 +18,29 @@ namespace Sample.Hosting.WebApi.Controllers {
 			return DateTime.UtcNow.ToString();
 		}
 
+		[HttpGet("datetime")]
+		public void Get([FromQuery]DateTime datetime) {
+			logger.LogInformation("kind: {kind}", datetime.Kind);
+			logger.LogInformation("value: {value:yyyy-MM-ddTHH:mm:sszzz}", datetime);
+		}
+
+		[HttpGet("datetimeoffset")]
+		public void Get([FromQuery] DateTimeOffset datetime) {
+			logger.LogInformation("value: {value:yyyy-MM-ddTHH:mm:sszzz}", datetime);
+		}
+
+		[HttpGet("datetime-from-route/{datetime1}/{id}/{datetime2}")]
+		public void Get([FromRoute] DateTime datetime1,[FromRoute] int id, [FromRoute] DateTime datetime2) {
+			logger.LogInformation("value: {value:yyyy-MM-ddTHH:mm:sszzz}", datetime1);
+			logger.LogInformation("value: {value:yyyy-MM-ddTHH:mm:sszzz}", datetime2);
+		}
+
+		[HttpGet("nullable-route/{id1}/{id2}/{id3}")]
+		public string Get(int?id1, int? ID2, int id3){
+			logger.LogInformation("{id1}, {id2}, {id3}", id1, ID2, id3);
+			return $"{id1}.{ID2}.{id3}";
+		}
+
 
 		[HttpGet("error")]
 		public ActionResult<string> TestError(string msg) {
--- webclient/Albatross.WebClient.Test/TestSerialization.cs
+++ webclient/Albatross.WebClient.Test/TestSerialization.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using Moq;
 using Albatross.Serialization;
+using System;
+using FluentAssertions;
 
 namespace Albatross.WebClient.Test {
 	public class TestClient : ClientBase {
@@ -44,6 +46,42 @@ namespace Albatross.WebClient.Test {
 			var client = new TestClient(new Mock<ILogger>().Object, new HttpClient(), new DefaultJsonSettings());
 			var result = client.Deserialize<ServiceError>(text);
 			Assert.NotNull(result);
+		}
+
+		[Fact]
+		public void TestDateTimeSerialization() {
+			var local = new DateTime(2024, 9, 12, 14, 30, 44, DateTimeKind.Local);
+			var utc = DateTime.SpecifyKind(local, DateTimeKind.Utc);
+			var unspecified = DateTime.SpecifyKind(local, DateTimeKind.Unspecified);
+
+			var localText_expected = JsonSerializer.Serialize(local);
+			var utcText_expected = JsonSerializer.Serialize(utc);
+			var unspecifiedText_expected = JsonSerializer.Serialize(unspecified);
+
+			local.JsonString().Should().Be(localText_expected.Trim('"'));
+			utc.JsonString().Should().Be(utcText_expected.Trim('"'));
+			unspecified.JsonString().Should().Be(unspecifiedText_expected.Trim('"'));
+		}
+
+		[Fact]
+		public void TestDateTimeOffsetSerialization() {
+			var value = new DateTimeOffset(2024, 9, 12, 14, 30, 44, TimeSpan.FromHours(3));
+			var value_expected = JsonSerializer.Serialize(value);
+			value.JsonString().Should().Be(value_expected.Trim('"'));
+		}
+
+		[Fact]
+		public void TestDateOnly() {
+			var value = new DateOnly(2024, 9, 12);
+			var value_expected = JsonSerializer.Serialize(value);
+			$"{value:yyyy-MM-dd}".Should().Be(value_expected.Trim('"'));
+		}
+
+		[Fact]
+		public void TestTimeOnly() {
+			var value = new TimeOnly(14, 20, 40, 123, 450);
+			var value_expected = JsonSerializer.Serialize(value);
+			$"{value:HH:mm:ss.fffffff}".Should().Be(value_expected.Trim('"'));
 		}
 	}
 }
