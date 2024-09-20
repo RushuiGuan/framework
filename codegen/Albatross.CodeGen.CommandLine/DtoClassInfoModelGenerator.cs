c621e714f8e9cbcc3c70a7911d0c8984a9e7eae6
Rushui <rushui@outlook.com>
2024-09-19 21:48:46 -0400

    wip

---
+++ codegen/Albatross.CodeGen.CommandLine/DtoClassInfoModelGenerator.cs
@@ -0,0 +1,81 @@
+﻿using Albatross.CodeGen.WebClient;
+using Albatross.CodeGen.WebClient.Models;
+using Albatross.CodeGen.WebClient.Settings;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Albatross.CodeGen.CommandLine {
+	public class DtoClassInfoModelGenerator : ICommandHandler {
+		private Compilation compilation;
+		private ConvertClassSymbolToDtoClassModel dtoConverter;
+		private readonly ConvertEnumSymbolToDtoEnumModel enumConverter;
+		private CodeGenSettings settings;
+		private CodeGenCommandOptions options;
+
+		public DtoClassInfoModelGenerator(Compilation compilation, ConvertClassSymbolToDtoClassModel dtoConverter,
+			ConvertEnumSymbolToDtoEnumModel enumConverter,
+			CodeGenSettings settings,
+			IOptions<CodeGenCommandOptions> options) {
+			this.compilation = compilation;
+			this.dtoConverter = dtoConverter;
+			this.enumConverter = enumConverter;
+			this.settings = settings;
+			this.options = options.Value;
+		}
+
+		public int Invoke(InvocationContext context) {
+			throw new System.NotImplementedException();
+		}
+
+		public Task<int> InvokeAsync(InvocationContext context) {
+			var dtoClasses = new List<INamedTypeSymbol>();
+			var enumClasses = new List<INamedTypeSymbol>();
+			foreach (var syntaxTree in compilation.SyntaxTrees) {
+				var semanticModel = compilation.GetSemanticModel(syntaxTree);
+				var symbolWalker = new DtoClassEnumWalker(semanticModel, settings.DtoFilter);
+				symbolWalker.Visit(syntaxTree.GetRoot());
+				dtoClasses.AddRange(symbolWalker.DtoClasses);
+				enumClasses.AddRange(symbolWalker.EnumTypes);
+			}
+			var serializationOptions = new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase, };
+			var dtoModels = new List<DtoClassInfo>();
+			foreach (var item in dtoClasses) {
+				if (string.IsNullOrEmpty(options.AdhocFilter) || string.Equals(item.Name, options.AdhocFilter, System.StringComparison.OrdinalIgnoreCase)) {
+					var model = dtoConverter.Convert(item);
+					dtoModels.Add(model);
+				}
+			}
+			var text = JsonSerializer.Serialize(dtoModels, serializationOptions);
+			System.Console.WriteLine(text);
+			var enumModels = new List<EnumInfo>();
+			foreach (var item in enumClasses){
+				if (string.IsNullOrEmpty(options.AdhocFilter) || string.Equals(item.Name, options.AdhocFilter, System.StringComparison.OrdinalIgnoreCase)) {
+					var model = enumConverter.Convert(item);
+					enumModels.Add(model);
+				}
+			}
+			text = JsonSerializer.Serialize(enumModels, serializationOptions);
+			System.Console.WriteLine(text);
+
+			if (options.OutputDirectory != null) {
+				if (dtoModels.Any()) {
+					using (var stream = File.OpenWrite(Path.Join(options.OutputDirectory.FullName, "dto.generated.json"))) {
+						JsonSerializer.SerializeAsync(stream, dtoModels, serializationOptions);
+					}
+				}
+				if(enumModels.Any()) {
+					using (var stream = File.OpenWrite(Path.Join(options.OutputDirectory.FullName, "enum.generated.json"))) {
+						JsonSerializer.SerializeAsync(stream, enumModels, serializationOptions);
+					}
+				}
+			}
+			return Task.FromResult(0);
+		}
+	}
+}
