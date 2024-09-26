2ffe500017d771c39b7fc6d7e27d72e0aa8714e5
Rushui <rushui@outlook.com>
2024-09-26 08:15:23 -0400

    upgrade commandline

---
+++ commandline/Sample.CommandLine/BadHandlerCommandOptions.cs
@@ -0,0 +1,22 @@
+﻿using Albatross.CommandLine;
+using System;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace Sample.CommandLine {
+	[Verb("bad-handler", typeof(BadHandlerCommandHandler))]
+	public class BadHandlerCommandOptions {
+	}
+	public class BadHandlerCommandHandler : ICommandHandler{
+		public BadHandlerCommandHandler(string input) {
+		}
+
+		public int Invoke(InvocationContext context) {
+			throw new NotImplementedException();
+		}
+
+		public Task<int> InvokeAsync(InvocationContext context) {
+			throw new NotImplementedException();
+		}
+	}
+}
--- commandline/Sample.CommandLine/SysCommandHandler.cs
+++ commandline/Sample.CommandLine/SysCommandHandler.cs
@@ -4,8 +4,26 @@ using System.CommandLine.Invocation;
 using System.CommandLine;
 using System.Threading.Tasks;
 using System;
+using Albatross.CommandLine;
 
 namespace Sample.CommandLine {
+	[Verb("sys-command", typeof(SysCommandHandler), Alias = ["t"])]
+	public record class SysCommandOptions {
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+
+		[Option(Required = false)]
+		public decimal Price { get; set; }
+
+		[Option(Skip = true)]
+		public int ShouldSkip { get; set; }
+
+		[Option(Skip = false)]
+		public int ShouldNotSkip { get; set; }
+
+		[Option(Required = true)]
+		public int? ForceRequired { get; set; }
+	}
 	public class SysCommandHandler : ICommandHandler {
 		private readonly ILogger<SysCommandHandler> logger;
 		private readonly IConsole console;
