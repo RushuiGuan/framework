eb9a648ddfb2f157b458ad2b89dbf7c8182ca3d8
rushuiguan <rushui@outlook.com>
2024-10-08 18:32:25 -0400

    update dates project

---
+++ dates/Albatross.Dates/Formatting.cs
@@ -0,0 +1,7 @@
+﻿namespace Albatross.Dates {
+	public static class Formatting {
+		public const string ISO8601DateOnly = "yyyy-MM-dd";
+		public const string ISO8601TimeOnly = "HH:mm:ss.fffffff";
+		public const string ISO8601 = "yyyy-MM-ddTHH:mm:ssK";
+	}
+}
