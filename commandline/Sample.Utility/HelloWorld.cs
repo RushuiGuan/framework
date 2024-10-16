c907b95f3ce7e6fb75d3e747bd32d2cad79f008c
rushuiguan <rushui@outlook.com>
2024-10-16 10:21:27 -0400

    legacy code clean up

--- commandline/Sample.Utility/HelloWorld.cs
+++ commandline/Sample.Utility/HelloWorld.cs
@@ -1,40 +1,18 @@
-﻿using Albatross.Hosting.CommandLine;
-using Microsoft.Extensions.Options;
-using System;
-using System.CommandLine;
-using System.CommandLine.Invocation;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Albatross.Hosting.Utility;
+using CommandLine;
 
 namespace Sample.Hosting.Utility {
 	public enum MyEnum {
 		One, Two, Three
 	}
 
-
-	//public partial class HelloWorldCommand : Command{
-	//	public HelloWorldCommand() : base("hello-world", "Say hello to the world") {
-			//this.AddOption(new Option<string>("--mu", "The mu value") {
-			//	IsRequired = true,
-			//	IsHidden = false,
-			//	AllowMultipleArgumentsPerToken = false,
-			//}.WithAlias("-m", "--m").FromAmong("a", "b", "c"));
-//	}
-//}
-
-
-[Verb("hello-world")]
+	[Verb("hello-world")]
 	public class HelloWorldOptions {
 		[Option("mu", Required = true)]
 		public string Mu { get; set; } = string.Empty;
 	}
-	public class HelloWorldCommandHandler : ICommandHandler{
-		public HelloWorldCommandHandler(IOptions<HelloWorldOptions> option) { }
-		
-		public int Invoke(InvocationContext context)=> throw new NotSupportedException();
-
-		public Task<int> InvokeAsync(InvocationContext context) {
-			throw new NotImplementedException();
+	public class HelloWorldCommandHandler : UtilityBase<HelloWorldOptions> {
+		public HelloWorldCommandHandler(HelloWorldOptions option) : base(option) {
 		}
 	}
 }
