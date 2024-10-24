ade6eb59529565e25d31475d8e366918411ae30d
Rushui <rushui@outlook.com>
2024-10-24 19:19:41 -0400

    add binary search

---
+++ collections/Albatross.Collections.Test/TestSearch.cs
@@ -0,0 +1,68 @@
+﻿using Albatross.EFCore;
+using Albatross.Hosting.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Albatross.Collections.Test {
+	public class TestSearch {
+
+		[Theory]
+		[InlineData("1-10", 5, 5)]
+		[InlineData("1-10", 0, 1)]
+		[InlineData("1-10", 11, null)]
+		[InlineData("1,3,5,7,9,11", 4, 5)]
+		[InlineData("1,10", 4, 10)]
+		public void TestGreaterOrEqualSearch_Value(string text, int target, int? expected) {
+			var array = text.IntArray();
+			var value = array.BinarySearchFirstValueGreaterOrEqual<int, int>(target, x => x);
+			Assert.Equal(expected, value);
+		}
+
+		[Theory]
+		[InlineData("1-10", 5, 5)]
+		[InlineData("1-10", 0, null)]
+		[InlineData("1-10", 11, 10)]
+		[InlineData("1,3,5,7,9,11", 4, 3)]
+		[InlineData("1,10", 4, 1)]
+		public void TestLessOrEqualSearch_Value(string text, int target, int? expected) {
+			var array = text.IntArray();
+			var value = array.BinarySearchFirstValueLessOrEqual<int, int>(target, x => x);
+			Assert.Equal(expected, value);
+		}
+
+		public class TestClass {
+			public TestClass(int value) {
+				this.Value = value;
+			}
+			public int Value { get; set; }
+		}
+
+		[Theory]
+		[InlineData("1-10", 5, 5)]
+		[InlineData("1-10", 0, 1)]
+		[InlineData("1-10", 11, null)]
+		[InlineData("1,3,5,7,9,11", 4, 5)]
+		[InlineData("1,10", 4, 10)]
+		public void TestGreaterOrEqualSearch(string text, int target, int? expected) {
+			var array = text.IntArray().Select(x => new TestClass(x)).ToArray();
+			var value = array.BinarySearchFirstGreaterOrEqual(target, x => x.Value);
+			Assert.Equal(expected, value?.Value);
+		}
+
+		[Theory]
+		[InlineData("1-10", 5, 5)]
+		[InlineData("1-10", 0, null)]
+		[InlineData("1-10", 11, 10)]
+		[InlineData("1,3,5,7,9,11", 4, 3)]
+		[InlineData("1,10", 4, 1)]
+		public void TestLessOrEqualSearch(string text, int target, int? expected) {
+			var array = text.IntArray().Select(x => new TestClass(x)).ToArray();
+			var value = array.BinarySearchFirstLessOrEqual(target, x => x.Value);
+			Assert.Equal(expected, value?.Value);
+		}
+	}
+}
---
+++ collections/Albatross.Collections/SearchExtensions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Albatross.Collections {
+	public static class SearchExtensions {
+		public static T? BinarySearchFirstValueGreaterOrEqual<T, K>(this IList<T> list, K key, Func<T, K> selector) where T : struct where K : IComparable<K> {
+			int left = 0;
+			int right = list.Count - 1;
+			T? result = default;
+
+			while (left <= right) {
+				int mid = left + (right - left) / 2;
+				T item = list[mid];
+				K value = selector(item);
+
+				if (value.CompareTo(key) >= 0) {
+					result = item;
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			}
+			return result;
+		}
+		public static T? BinarySearchFirstValueLessOrEqual<T, K>(this IList<T> list, K key, Func<T, K> selector)
+			where T : struct
+			where K : IComparable<K> {
+			int left = 0;
+			int right = list.Count - 1;
+			T? result = default;
+
+			while (left <= right) {
+				int mid = left + (right - left) / 2;
+				T item = list[mid];
+				K value = selector(item);
+
+				if (value.CompareTo(key) <= 0) {
+					result = item;
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+
+			return result;
+		}
+
+		public static T? BinarySearchFirstGreaterOrEqual<T, K>(this IList<T> list, K key, Func<T, K> selector)
+			where T : class where K : IComparable<K> {
+			int left = 0;
+			int right = list.Count - 1;
+			T? result = default;
+
+			while (left <= right) {
+				int mid = left + (right - left) / 2;
+				T item = list[mid];
+				K value = selector(item);
+
+				if (value.CompareTo(key) >= 0) {
+					result = item;
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			}
+			return result;
+		}
+		public static T? BinarySearchFirstLessOrEqual<T, K>(this IList<T> list, K key, Func<T, K> selector)
+			where T : class
+			where K : IComparable<K> {
+			int left = 0;
+			int right = list.Count - 1;
+			T? result = default;
+
+			while (left <= right) {
+				int mid = left + (right - left) / 2;
+				T item = list[mid];
+				K value = selector(item);
+
+				if (value.CompareTo(key) <= 0) {
+					result = item;
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+
+			return result;
+		}
+	}
+}
