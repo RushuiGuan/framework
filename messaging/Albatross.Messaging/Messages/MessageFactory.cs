14ed2f1fbf7c977f80cd1295480cc43fb3d112e5
Rushui <rushui@outlook.com>
2024-10-28 01:23:30 -0400

    remove reflection usage in messaging

--- messaging/Albatross.Messaging/Messages/MessageFactory.cs
+++ messaging/Albatross.Messaging/Messages/MessageFactory.cs
@@ -1,4 +1,7 @@
-﻿using Albatross.Messaging.EventSource;
+﻿using Albatross.Messaging.Commands.Messages;
+using Albatross.Messaging.EventSource;
+using Albatross.Messaging.PubSub.Messages;
+using Albatross.Messaging.ReqRep.Messages;
 using Albatross.Reflection;
 using Microsoft.Extensions.Logging;
 using NetMQ;
@@ -10,14 +13,36 @@ using System.Reflection;
 namespace Albatross.Messaging.Messages {
 	public class MessageFactory : IMessageFactory {
 		Dictionary<string, IMessageBuilder> builders = new Dictionary<string, IMessageBuilder>();
+		void Add(IMessageBuilder builder) {
+			this.builders.Add(builder.Header, builder);
+		}
 
 		public MessageFactory() {
-			var types = Assembly.GetExecutingAssembly().GetConcreteClasses<IMessage>();
-			foreach (var type in types) {
-				Type genericType = typeof(MessageBuilder<>).MakeGenericType(type) ?? throw new NotSupportedException();
-				var builder = (IMessageBuilder)(Activator.CreateInstance(genericType) ?? throw new NotSupportedException());
-				this.builders.Add(builder.Header, builder);
-			}
+			Add(new MessageBuilder<CommandErrorReply>());
+			Add(new MessageBuilder<CommandReply>());
+			Add(new MessageBuilder<CommandRequest>());
+			Add(new MessageBuilder<CommandRequestAck>());
+			Add(new MessageBuilder<CommandRequestError>());
+			Add(new MessageBuilder<ClientAck>());
+			Add(new MessageBuilder<Connect>());
+			Add(new MessageBuilder<ConnectOk>());
+			Add(new MessageBuilder<Heartbeat>());
+			Add(new MessageBuilder<HeartbeatAck>());
+			Add(new MessageBuilder<Reconnect>());
+			Add(new MessageBuilder<Resume>());
+			Add(new MessageBuilder<ServerAck>());
+			Add(new MessageBuilder<UnknownMsg>());
+			Add(new MessageBuilder<Event>());
+			Add(new MessageBuilder<SubscriptionReply>());
+			Add(new MessageBuilder<SubscriptionRequest>());
+			Add(new MessageBuilder<UnsubscribeAllRequest>());
+			Add(new MessageBuilder<BrokerAck>());
+			Add(new MessageBuilder<BrokerRequest>());
+			Add(new MessageBuilder<BrokerResponse>());
+			Add(new MessageBuilder<ClientRequest>());
+			Add(new MessageBuilder<NoAvailableWorker>());
+			Add(new MessageBuilder<WorkerAck>());
+			Add(new MessageBuilder<WorkerResponse>());
 		}
 		public IMessage Create(NetMQMessage frames) {
 			var header = frames.PeekMessageHeader();
