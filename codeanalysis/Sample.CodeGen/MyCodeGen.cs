cb212aee6dcfdb502c341354fcd8a024890af113
Rushui <rushui@outlook.com>
2024-11-13 23:44:00 -0500

    update code analysis interface builder

--- codeanalysis/Sample.CodeGen/MyCodeGen.cs
+++ codeanalysis/Sample.CodeGen/MyCodeGen.cs
@@ -14,7 +14,7 @@ namespace Sample.CodeGen {
 			using (cs.NewScope(new CompilationUnitBuilder())) {
 				using (cs.NewScope(new NamespaceDeclarationBuilder("Sample.CodeGen"))) {
 					using (cs.NewScope(new ClassDeclarationBuilder("MyTest"))) {
-						using (cs.NewScope(new MethodDeclarationBuilder("void", "MyMethod"))) {
+						using (cs.NewScope(new MethodDeclarationBuilder("void", "MyMethod").Public())) {
 							cs.Begin(new VariableBuilder("string", "test1")).With(new LiteralNode("MyTest".Kebaberize())).End();
 						}
 					}
