1d5296b978d4a0361eff58d1454c584ad5f0c7dd
Rushui <rushui@outlook.com>
2024-12-08 01:49:38 -0500

    update documentation

---
+++ commandline/Sample.CommandLine/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using Albatross.CommandLine;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Sample.CommandLine {
+	[Verb("hello", typeof(HelloWorldCommandHandler), Description = "HelloWorld command")]
+	public record class HelloWorldOptions {
+		[Option("n", Description = "Give me a name")]
+		public string Name { get; set; } = string.Empty;
+
+		[Option("d", Description = "Give me an optional date")]
+		public DateOnly? Date { get; set; }
+
+		[Option("x", Description = "Give me a number", DefaultToInitializer = true)]
+		public int Number { get; set; } = 100;
+	}
+	public class HelloWorldCommandHandler : BaseHandler<HelloWorldOptions> {
+		public HelloWorldCommandHandler(IOptions<HelloWorldOptions> options) : base(options) {
+		}
+	}
+}
