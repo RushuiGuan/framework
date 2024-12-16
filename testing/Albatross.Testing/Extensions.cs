8f3d2a0761984b2e1a845a06db51520dac30ddd5
rushuiguan <rushui@outlook.com>
2024-12-16 12:33:16 -0500

    add comment

---
+++ testing/Albatross.Testing/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Albatross.Testing {
+	public static class Extensions {
+		public static IEnumerable<object[]> ConvertToTheory<T>(this IEnumerable<T> source) where T : notnull {
+			foreach (var item in source) {
+				yield return new object[] { item };
+			}
+		}
+	}
+}
