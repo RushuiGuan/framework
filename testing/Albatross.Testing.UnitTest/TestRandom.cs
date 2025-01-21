1ac7d1fb581166f4603981ef60e97a8734927edc
rushuiguan <rushui@outlook.com>
2025-01-21 12:46:35 -0500

    fix up random array code

---
+++ testing/Albatross.Testing.UnitTest/TestRandom.cs
@@ -0,0 +1,15 @@
+﻿using Albatross.Testing;
+using FluentAssertions;
+using Xunit;
+
+namespace Sample.Hosting.Test {
+	public class TestRandom {
+		[Fact]
+		public void TestRandomArray() {
+			var array = new int[] { 
+				1, 2, 3,4
+			};
+			array.Random().Should().BeOneOf(array);
+		}
+	}
+}
--- testing/Albatross.Testing/RandomExtensions.cs
+++ testing/Albatross.Testing/RandomExtensions.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Albatross.Testing {
 	public static class RandomExtensions {
-		public static T Random<T>(this T[] items) {
-			return items[System.Random.Shared.Next(0, items.Length)];
+		public static T Random<T>(this IList<T> items) {
+			return items[System.Random.Shared.Next(0, items.Count)];
 		}
 
 		public static DateOnly RandomDates(DateOnly min, DateOnly max) {
