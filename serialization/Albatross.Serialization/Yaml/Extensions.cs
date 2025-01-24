4aa2bf0b5dc1ec9cd5bfb6416f6c51001cc9a99c
Rushui <rushui@outlook.com>
2025-01-23 19:28:07 -0500

    fix yaml serialization setup

--- serialization/Albatross.Serialization/Yaml/Extensions.cs
+++ serialization/Albatross.Serialization/Yaml/Extensions.cs
@@ -3,16 +3,16 @@ using YamlDotNet.Serialization;
 
 namespace Albatross.Serialization.Yaml {
 	public static class Extensions {
-		public static ISerializer DefaultYamlSerializer() {
-			var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance)
+		public static SerializerBuilder DefaultBuilder(this SerializerBuilder builder) =>
+			builder.WithNamingConvention(CamelCaseNamingConvention.Instance)
 				.ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitDefaults)
 #if NET6_0_OR_GREATER
 				.WithTypeConverter(new DateOnlyTypeConverter())
 #endif
 				.WithTypeConverter(new DateTimeTypeConverter())
-				.WithTypeConverter(new JsonElementTypeConverter())
-				.Build();
-			return serializer;
-		}
+				.WithTypeConverter(new JsonElementTypeConverter());
+
+		public static ISerializer DefaultSerializer(this SerializerBuilder builder)
+			=> builder.DefaultBuilder().Build();
 	}
 }
