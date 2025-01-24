640d1f4eda51e8358b32a724adc5e31c31235b8c
Rushui <rushui@outlook.com>
2025-01-23 19:29:50 -0500

    fix yaml serialization setup

--- serialization/Albatross.Serialization.Test/TestYamlSerializer.cs
+++ serialization/Albatross.Serialization.Test/TestYamlSerializer.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Albatross.Serialization.Yaml;
+using System;
 using System.Text.Json;
 using Xunit;
+using YamlDotNet.Serialization;
 
 namespace Albatross.Serialization.Test {
 	public class TestYamlSerializer {
@@ -10,14 +12,14 @@ namespace Albatross.Serialization.Test {
 		[InlineData("2021-01-01Z", "2021-01-01T00:00:00Z")]
 		public void TestDateTimeSerialization(string value, string expected) {
 			var date = DateTime.Parse(value, null, System.Globalization.DateTimeStyles.RoundtripKind);
-			Assert.Equal(expected, Yaml.Extensions.DefaultYamlSerializer().Serialize(date).Trim());
+			Assert.Equal(expected, new SerializerBuilder().DefaultSerializer().Serialize(date).Trim());
 		}
 
 		[Theory]
 		[InlineData("2021-01-01", "2021-01-01")]
 		public void TestDateOnlySerialization(string value, string expected) {
 			var date = DateOnly.Parse(value);
-			Assert.Equal(expected, Yaml.Extensions.DefaultYamlSerializer().Serialize(date).Trim());
+			Assert.Equal(expected, new SerializerBuilder().DefaultSerializer().Serialize(date).Trim());
 		}
 
 		[Theory]
@@ -25,7 +27,7 @@ namespace Albatross.Serialization.Test {
 		[InlineData("\"a\"", "'a'")]
 		public void TestJsonElementSerialization(string text, string expected) {
 			var value = JsonDocument.Parse(text).RootElement;
-			var result = Yaml.Extensions.DefaultYamlSerializer().Serialize(value).Trim();
+			var result = new SerializerBuilder().DefaultSerializer().Serialize(value).Trim();
 			Assert.Equal(expected, result);
 		}
 	}
